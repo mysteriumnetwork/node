2fe311d15a938d61e0e129c816ebf64167e353b0
zyfdegh <zyfdegg@gmail.com>
2017-05-29 16:34:29 +0800

    use filepath.Join to concat path; fix loop err

--- openvpn/utils.go
+++ openvpn/utils.go
@@ -4,6 +4,7 @@ import (
 	"time"
 	"os"
 	"math/rand"
+	"path/filepath"
 )
 
 var seededRand *rand.Rand = rand.New(
@@ -14,9 +15,9 @@ const finenameCharset = "1234567890abcdefghijklmnopqrstuvwxyz"
 
 func tempFilename(directory, filePrefix, fileExtension string) (filePath string) {
 	for i := 0; i < 10000; i++ {
-		filePath = directory + "/" + filePrefix + randomString(10, finenameCharset) + fileExtension
-		if _, err := os.Stat(filePath); os.IsExist(err) {
-			continue
+		filePath = filepath.Join(directory, filePrefix, randomString(10, finenameCharset), fileExtension)
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			break
 		}
 	}
 	return
