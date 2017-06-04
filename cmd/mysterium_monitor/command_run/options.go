418823325d81e29cadf9648439a5bc049bb29f9f
Valdas Petrulis <petrulis.valdas@gmail.com>
2017-06-04 22:42:54 +0300

    CLI argument "node-file" to monitor list of nodes

--- cmd/mysterium_monitor/command_run/options.go
+++ cmd/mysterium_monitor/command_run/options.go
@@ -1,8 +1,10 @@
 package command_run
 
 import (
+	"bufio"
 	"errors"
 	"flag"
+	"os"
 )
 
 type CommandOptions struct {
@@ -24,7 +26,15 @@ func ParseArguments(args []string) (options CommandOptions, err error) {
 		&nodeToCheck,
 		"node",
 		"",
-		"Which node should be checked",
+		"Node to be checked",
+	)
+
+	var fileToCheck string
+	flags.StringVar(
+		&fileToCheck,
+		"node-file",
+		"",
+		"File with node list to be checked",
 	)
 
 	err = flags.Parse(args[1:])
@@ -34,9 +44,26 @@ func ParseArguments(args []string) (options CommandOptions, err error) {
 
 	if nodeToCheck != "" {
 		options.NodeKeys = []string{nodeToCheck}
+	} else if fileToCheck != "" {
+		options.NodeKeys, err = parseLines(fileToCheck)
 	} else {
 		err = errors.New("Provide which nodes to monitor!")
 	}
 
 	return options, err
+}
+
+func parseLines(filePath string) (lines []string, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	file.Close()
+	return
 }
