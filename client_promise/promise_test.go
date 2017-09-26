4d5611694615197857b30e5035f632397d737599
Paulius <interro@gmail.com>
2017-09-26 10:57:45 +0300

    test for amount

--- client_promise/promise_test.go
+++ client_promise/promise_test.go
@@ -1,21 +1,29 @@
 package client_promise
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
 	dto "github.com/mysterium/node/client_promise/dto"
+	discovery_dto "github.com/mysterium/node/service_discovery/dto"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func Test_ClientPromise(t *testing.T) {
+
+	amount := discovery_dto.Money{
+		Amount:   uint64(5),
+		Currency: "Token",
+	}
+
 	promise := dto.PromiseBody{
 		SerialNumber: 1,
-		IssuerId: "issuer1",
-		BenefiterId: "benefiter1",
-		Amount: 5,
+		IssuerId:     "issuer1",
+		BenefiterId:  "benefiter1",
+		Amount:       amount,
 	}
 
 	assert.Equal(t, promise.SerialNumber, 1)
 	assert.Equal(t, promise.IssuerId, "issuer1")
 	assert.Equal(t, promise.BenefiterId, "benefiter1")
-	assert.Equal(t, promise.Amount, 5)
+	assert.Equal(t, promise.Amount.Amount, uint64(5))
+	assert.Equal(t, promise.Amount.Currency, "Token")
 }
