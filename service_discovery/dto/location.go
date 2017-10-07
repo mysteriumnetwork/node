2bde3276c2895432bae750e1e93341536c565ee7
Waldz <valdas@mysterium.network>
2017-10-07 16:33:06 +0300

    Location may have optional fields

--- service_discovery/dto/location.go
+++ service_discovery/dto/location.go
@@ -1,8 +1,8 @@
 package dto
 
 type Location struct {
-	Country string `json:"country"`
-	City    string `json:"city"`
+	Country string `json:"country,omitempty"`
+	City    string `json:"city,omitempty"`
 	// Autonomous System Number http://www.whatismyip.cx/
-	ASN string `json:"asn"`
+	ASN string `json:"asn,omitempty"`
 }
--- service_discovery/dto/location_test.go
+++ service_discovery/dto/location_test.go
@@ -2,29 +2,89 @@ package dto
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var (
-	locationLTTelia = Location{"XX", "YY", "AS123"}
-	locationJson    = `{
-		"country": "XX",
-		"city": "YY",
-		"asn":"AS123"
-	}`
-)
-
 func TestLocationSerialize(t *testing.T) {
-	jsonBytes, err := json.Marshal(locationLTTelia)
-	assert.Nil(t, err)
-	assert.JSONEq(t, locationJson, string(jsonBytes))
+	var tests = []struct {
+		model        Location
+		expectedJson string
+	}{
+		{
+			Location{"XX", "YY", "AS123"},
+			`{
+				"country": "XX",
+				"city": "YY",
+				"asn": "AS123"
+			}`,
+		},
+		{
+			Location{Country: "XX"},
+			`{
+				"country": "XX"
+			}`,
+		},
+		{
+			Location{},
+			`{}`,
+		},
+	}
+
+	for _, test := range tests {
+		jsonBytes, err := json.Marshal(test.model)
+
+		assert.Nil(t, err)
+		assert.JSONEq(t, test.expectedJson, string(jsonBytes))
+	}
 }
 
 func TestLocationUnserialize(t *testing.T) {
-	var model Location
+	var tests = []struct {
+		json          string
+		expectedModel Location
+		expectedError error
+	}{
+		{
+			`{
+				"country": "XX",
+				"city": "YY",
+				"asn": "AS123"
+			}`,
+			Location{"XX", "YY", "AS123"},
+			nil,
+		},
+		{
+			`{
+				"country": "XX"
+			}`,
+			Location{Country: "XX"},
+			nil,
+		},
+		{
+			`{}`,
+			Location{},
+			nil,
+		},
+		{
+			`{
+				"country": 1
+			}`,
+			Location{},
+			errors.New("json: cannot unmarshal number into Go struct field Location.country of type string"),
+		},
+	}
 
-	err := json.Unmarshal([]byte(locationJson), &model)
-	assert.Nil(t, err)
-	assert.Equal(t, locationLTTelia, model)
+	for _, test := range tests {
+		var model Location
+		err := json.Unmarshal([]byte(test.json), &model)
+
+		assert.Equal(t, test.expectedModel, model)
+		if test.expectedError != nil {
+			assert.EqualError(t, err, test.expectedError.Error())
+		} else {
+			assert.NoError(t, err)
+		}
+	}
 }
