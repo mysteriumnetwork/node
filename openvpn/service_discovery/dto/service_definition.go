19497217475552b2886fa5dc0728af65efbb47e7
Waldz <valdas@mysterium.network>
2017-10-07 17:03:03 +0300

    VPN ServiceDefinition to JSON

--- openvpn/service_discovery/dto/service_definition.go
+++ openvpn/service_discovery/dto/service_definition.go
@@ -6,13 +6,13 @@ import (
 
 type ServiceDefinition struct {
 	// Approximate information on location where the service is provided from
-	Location dto_discovery.Location
+	Location dto_discovery.Location `json:"location"`
 
 	// Approximate information on location where the tunnelled traffic will originate from
-	LocationOriginate dto_discovery.Location
+	LocationOriginate dto_discovery.Location `json:"location_originate"`
 
 	// Available per session bandwidth
-	SessionBandwidth Bandwidth
+	SessionBandwidth Bandwidth `json:"session_bandwidth,omitempty"`
 }
 
 func (service ServiceDefinition) GetLocation() dto_discovery.Location {
