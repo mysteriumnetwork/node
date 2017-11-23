99940a0095dc69a8a46517b604aa48ff380b466a
Paulius <interro@gmail.com>
2017-11-23 14:59:01 +0200

    assert.Len(...)

--- identity/manager_test.go
+++ identity/manager_test.go
@@ -12,7 +12,7 @@ func Test_CreateNewIdentity(t *testing.T) {
 	manager := NewIdentityManager("testdata")
 	id, err := manager.CreateNewIdentity()
 	assert.NoError(t, err)
-	assert.Equal(t, len(id), 42)
+	assert.Len(t, id, 42)
 }
 
 func Test_GetIdentities(t *testing.T) {
@@ -53,7 +53,7 @@ func Test_SignMessage(t *testing.T) {
 	for _, id := range ids {
 		signature, err := manager.SignMessage(id, "message to sign")
 		assert.NoError(t, err)
-		assert.Equal(t, len(signature), 65)
+		assert.Len(t, signature, 65)
 	}
 }
 func Test_SignVerifyMessage(t *testing.T) {
