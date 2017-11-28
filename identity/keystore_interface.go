e5f5f5e7fb8cf232a20bac9a6278ef49623c121d
Paulius <interro@gmail.com>
2017-11-28 13:50:52 +0200

    unused interface declarations

--- identity/keystore_interface.go
+++ identity/keystore_interface.go
@@ -5,6 +5,4 @@ import "github.com/ethereum/go-ethereum/accounts"
 type keystoreManager interface {
 	Accounts() []accounts.Account
 	NewAccount(passphrase string) (accounts.Account, error)
-	Unlock(a accounts.Account, passphrase string) error
-	SignHash(a accounts.Account, hash []byte) ([]byte, error)
 }
