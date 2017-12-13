45c42b0f8304522a51cadd96577dc3562abec76b
Tadas V <tadovas@gmail.com>
2017-12-13 10:12:51 +0200

    Json renderer micro improvement

--- tequilapi/utils/utils.go
+++ tequilapi/utils/utils.go
@@ -12,14 +12,10 @@ also enforces application/json and charset response headers
 */
 func WriteAsJson(v interface{}, writer http.ResponseWriter) {
 
-	serialized, err := json.Marshal(v)
-	if err != nil {
-		http.Error(writer, "Unable to serialize response body", http.StatusInternalServerError)
-		return
-	}
 	writer.Header().Add("Content-type", "application/json")
 	writer.Header().Add("Content-type", "charset=utf-8")
-	_, writeErr := writer.Write(serialized)
+
+	writeErr := json.NewEncoder(writer).Encode(v)
 	if writeErr != nil {
 		http.Error(writer, "Http response write error", http.StatusInternalServerError)
 	}
