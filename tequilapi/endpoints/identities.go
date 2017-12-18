e8eb7867b2b007403aca7409378ef2ed98ffb5eb
Andrej N <jazzaiman@gmail.com>
2017-12-18 16:09:30 +0200

    identity create endpoint scaffold

--- tequilapi/endpoints/identities.go
+++ tequilapi/endpoints/identities.go
@@ -3,13 +3,23 @@ package endpoints
 import (
 	"net/http"
 
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"github.com/mysterium/node/identity"
 	"github.com/mysterium/node/tequilapi/utils"
+	"github.com/mysterium/node/tequilapi/validation"
 )
 
 type identityDto struct {
 	Id string `json:"id"`
+}
+
+type identityCreationDto struct {
+	Password string `json:"password"`
+}
+
+type identityRegistrationDto struct {
+	Registered bool `json:"registered"`
 }
 
 type identitiesApi struct {
@@ -22,7 +32,6 @@ func NewIdentitiesEndpoint(idm identity.IdentityManagerInterface) *identitiesApi
 
 func (endpoint *identitiesApi) List(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 	idArry := endpoint.idm.GetIdentities()
-
 	idsSerializable := make([]identityDto, len(idArry))
 	for i, id := range idArry {
 		idsSerializable[i] = identityDto{
@@ -33,6 +42,75 @@ func (endpoint *identitiesApi) List(writer http.ResponseWriter, request *http.Re
 	utils.WriteAsJson(idsSerializable, writer)
 }
 
+func (endpoint *identitiesApi) Create(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	createReq, err := toCreateRequest(request)
+	if err != nil {
+		utils.SendError(writer, err, http.StatusBadRequest)
+		return
+	}
+	errorMap := validateCreationRequest(createReq)
+	if errorMap.HasErrors() {
+		utils.SendValidationErrorMessage(writer, errorMap)
+		return
+	}
+	id, err := endpoint.idm.CreateNewIdentity(createReq.Password)
+	idDto := identityDto{string(*id)}
+	if err != nil {
+		utils.SendError(writer, err, http.StatusInternalServerError) // This should never happen
+		return
+	}
+
+	utils.WriteAsJson(idDto, writer)
+}
+
+func (endpoint *identitiesApi) Register(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	registerReq, err := toRegisterRequest(request)
+	if err != nil {
+		utils.SendError(writer, err, http.StatusBadRequest)
+		return
+	}
+	errorMap := validateRegistrationRequest(registerReq)
+	if errorMap.HasErrors() {
+		utils.SendValidationErrorMessage(writer, errorMap)
+		return
+	}
+
+	// TODO: Register here
+
+}
+
+func toCreateRequest(req *http.Request) (*identityCreationDto, error) {
+	var identityCreationReq = &identityCreationDto{}
+	err := json.NewDecoder(req.Body).Decode(&identityCreationReq)
+	if err != nil {
+		return nil, err
+	}
+	return identityCreationReq, nil
+}
+
+func toRegisterRequest(req *http.Request) (*identityRegistrationDto, error) {
+	var idRegDto = identityRegistrationDto{}
+	err := json.NewDecoder(req.Body).Decode(&idRegDto)
+	if err != nil {
+		return nil, err
+	}
+	return &idRegDto, nil
+}
+
+func validateRegistrationRequest(regReq *identityRegistrationDto) (errors *validation.FieldErrorMap) {
+	errors = validation.NewErrorMap()
+	return
+}
+
+func validateCreationRequest(createReq *identityCreationDto) (errors *validation.FieldErrorMap) {
+	errors = validation.NewErrorMap()
+	return
+}
+
 func RegisterIdentitiesEndpoint(router *httprouter.Router, idm identity.IdentityManagerInterface) {
-	router.GET("/identities", NewIdentitiesEndpoint(idm).List)
+	idmEnd := NewIdentitiesEndpoint(idm)
+	router.GET("/identities", idmEnd.List)
+	router.POST("/identities", idmEnd.Create)
+	//router.GET("/identities/:id/registration", idmEnd.IsRegister)
+	router.PUT("/identities/:id/registration", idmEnd.Register)
 }
--- tequilapi/endpoints/identities_test.go
+++ tequilapi/endpoints/identities_test.go
@@ -1,15 +1,79 @@
 package endpoints
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"bytes"
 	"github.com/mysterium/node/identity"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListIdentities(t *testing.T) {
+var identityUrl = "/identities/123/registration"
 
+func TestRegisterExistingIdentityRequest(t *testing.T) {
+	req, err := http.NewRequest(
+		http.MethodPut,
+		identityUrl,
+		bytes.NewBufferString(
+			`{
+				"registered": "false"
+			}`))
+	assert.Nil(t, err)
+	resp := httptest.NewRecorder()
+
+	mockIdm := identity.NewIdentityManagerFake()
+	handlerFunc := NewIdentitiesEndpoint(mockIdm).Register
+	handlerFunc(resp, req, nil)
+
+}
+
+func TestRegisterIdentitySuccess(t *testing.T) {
+	req, err := http.NewRequest(
+		http.MethodPut,
+		identityUrl,
+		bytes.NewBufferString(
+			`{
+				"registered": "true"
+			}`))
+	assert.Nil(t, err)
+	resp := httptest.NewRecorder()
+
+	mockIdm := identity.NewIdentityManagerFake()
+	handlerFunc := NewIdentitiesEndpoint(mockIdm).Register
+	handlerFunc(resp, req, nil)
+
+}
+
+func TestCreateNewIdentity(t *testing.T) {
+	req, err := http.NewRequest(
+		http.MethodPost,
+		"/identities",
+		bytes.NewBufferString(
+			`{
+				"password": "mypass"
+			}`))
+	assert.Nil(t, err)
+
+	resp := httptest.NewRecorder()
+
+	mockIdm := identity.NewIdentityManagerFake()
+	handlerFunc := NewIdentitiesEndpoint(mockIdm).Create
+	handlerFunc(resp, req, nil)
+
+	assert.JSONEq(
+		t,
+		`
+            {
+                "id":"0x000000000000000000000000000000000000bEEF"
+            }
+        `,
+		resp.Body.String(),
+	)
+}
+
+func TestListIdentities(t *testing.T) {
 	req := httptest.NewRequest("GET", "/irrelevant", nil)
 	resp := httptest.NewRecorder()
 
