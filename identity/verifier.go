c23b40cfebea69948181dd11c68d377023c52663
Tadas V <tadovas@gmail.com>
2017-12-27 14:08:11 +0200

    Verifier tests (1 failing)

--- identity/verifier.go
+++ identity/verifier.go
@@ -1,1 +1,23 @@
 package identity
+
+import "github.com/ethereum/go-ethereum/crypto"
+
+type Verifier interface {
+	Verify(message []byte, signature []byte) bool
+}
+
+type ethereumVerfier struct {
+}
+
+func NewVerifier() *ethereumVerfier {
+	return &ethereumVerfier{}
+}
+
+func (ev *ethereumVerfier) Verify(message []byte, signature []byte) bool {
+	hash := messageHash(message)
+	_, err := crypto.Ecrecover(hash, signature)
+	if err != nil {
+		return false
+	}
+	return true
+}
--- identity/verifier_test.go
+++ identity/verifier_test.go
@@ -1,1 +1,20 @@
 package identity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVerifyFunctionReturnsTrueWhenSignatureIsCorrect(t *testing.T) {
+	message := []byte("I am message!")
+	signature := []byte{1, 2, 3}
+
+	assert.True(t, NewVerifier().Verify(message, signature))
+}
+
+func TestVerifyFunctionReturnsFalseWhenSignatureIsIncorrect(t *testing.T) {
+	message := []byte("I am message!")
+	signature := []byte{1, 2, 3}
+
+	assert.False(t, NewVerifier().Verify(message, signature))
+}
