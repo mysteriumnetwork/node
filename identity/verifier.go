8956c05da6ed642a8ae8a5fb2ff64d8812c08510
Tadas V <tadovas@gmail.com>
2017-12-28 11:10:51 +0200

    Verifier with tests added

--- identity/verifier.go
+++ identity/verifier.go
@@ -1,23 +1,27 @@
 package identity
 
-import "github.com/ethereum/go-ethereum/crypto"
+import (
+	"github.com/ethereum/go-ethereum/crypto"
+)
 
 type Verifier interface {
 	Verify(message []byte, signature []byte) bool
 }
 
-type ethereumVerfier struct {
+type ethereumVerifier struct {
+	peerIdentity Identity
 }
 
-func NewVerifier() *ethereumVerfier {
-	return &ethereumVerfier{}
+func NewVerifier(peerIdentity Identity) *ethereumVerifier {
+	return &ethereumVerifier{peerIdentity}
 }
 
-func (ev *ethereumVerfier) Verify(message []byte, signature []byte) bool {
+func (ev *ethereumVerifier) Verify(message []byte, signature []byte) bool {
 	hash := messageHash(message)
-	_, err := crypto.Ecrecover(hash, signature)
+	pk, err := crypto.Ecrecover(hash, signature)
 	if err != nil {
 		return false
 	}
-	return true
+	recoveredAddress := crypto.PubkeyToAddress(*crypto.ToECDSAPub(pk)).Hex()
+	return FromAddress(recoveredAddress) == ev.peerIdentity
 }
--- identity/verifier_test.go
+++ identity/verifier_test.go
@@ -1,20 +1,42 @@
 package identity
 
 import (
+	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestVerifyFunctionReturnsTrueWhenSignatureIsCorrect(t *testing.T) {
 	message := []byte("I am message!")
-	signature := []byte{1, 2, 3}
 
-	assert.True(t, NewVerifier().Verify(message, signature))
+	privateKey, err := crypto.GenerateKey()
+	assert.NoError(t, err)
+
+	hash := messageHash(message)
+	signature, err := crypto.Sign(hash, privateKey)
+	assert.NoError(t, err)
+
+	identity := toIdentity(privateKey)
+	assert.True(t, NewVerifier(identity).Verify(message, signature))
 }
 
 func TestVerifyFunctionReturnsFalseWhenSignatureIsIncorrect(t *testing.T) {
 	message := []byte("I am message!")
-	signature := []byte{1, 2, 3}
+	privateKey, err := crypto.GenerateKey()
+	assert.NoError(t, err)
 
-	assert.False(t, NewVerifier().Verify(message, signature))
+	signature, err := crypto.Sign(crypto.Keccak256(message), privateKey)
+	assert.NoError(t, err)
+	//change message
+	message[1] = 'b'
+
+	assert.False(t, NewVerifier(toIdentity(privateKey)).Verify(message, signature))
+}
+
+func toIdentity(privKey *ecdsa.PrivateKey) Identity {
+
+	ecdsaPubKey := privKey.PublicKey
+
+	return FromAddress(crypto.PubkeyToAddress(ecdsaPubKey).Hex())
 }
