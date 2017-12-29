4a231f2465ceff3792dd4371ef234e611a7bcc6b
Andrej N <jazzaiman@gmail.com>
2017-12-29 18:10:27 +0200

    proposalsTequilaDto{Id, ProviderId, ServiceDefinition{LocationOriginate{ASN}}}

--- tequilapi/endpoints/proposals.go
+++ tequilapi/endpoints/proposals.go
@@ -1,38 +1,74 @@
 package endpoints
 
 import (
-	"github.com/julienschmidt/httprouter"
-	"github.com/mysterium/node/server"
-	dto_discovery "github.com/mysterium/node/service_discovery/dto"
-	"github.com/mysterium/node/tequilapi/utils"
-	"net/http"
+    "github.com/julienschmidt/httprouter"
+    "github.com/mysterium/node/server"
+    dto_discovery "github.com/mysterium/node/service_discovery/dto"
+    "github.com/mysterium/node/tequilapi/utils"
+    "net/http"
 )
 
-type proposalsEndpoint struct {
-	mysteriumClient server.Client
+type proposalsDto struct {
+    Proposals []proposalTeqDto `json:"proposals"`
 }
 
-type proposalsDto struct {
-	Proposals []dto_discovery.ServiceProposal `json:"proposals"`
+type locationTeqDto struct {
+    ASN string `json:"asn"`
+}
+type serviceDefinitionTeqDto struct {
+    LocationOriginate locationTeqDto `json:"locationOriginate"`
+}
+
+type proposalTeqDto struct {
+    Id                int                     `json:"id"`
+    ProviderId        string                  `json:"providerId"`
+    ServiceDefinition serviceDefinitionTeqDto `json:"serviceDefinition"`
+}
+
+func proposalToDto(p dto_discovery.ServiceProposal) proposalTeqDto {
+    return proposalTeqDto{
+        Id:         p.Id,
+        ProviderId: p.ProviderId,
+        ServiceDefinition: serviceDefinitionTeqDto{
+            LocationOriginate: locationTeqDto{
+                ASN: p.ServiceDefinition.GetLocation().ASN,
+            },
+        },
+    }
+}
+
+func mapProposalsToTeqDto(
+    proposalArry []dto_discovery.ServiceProposal,
+    f func(dto_discovery.ServiceProposal) proposalTeqDto,
+) []proposalTeqDto {
+    proposalsDtoArry := make([]proposalTeqDto, len(proposalArry))
+    for i, proposal := range proposalArry {
+        proposalsDtoArry[i] = f(proposal)
+    }
+    return proposalsDtoArry
+}
+
+type proposalsEndpoint struct {
+    mysteriumClient server.Client
 }
 
 func NewProposalsEndpoint(mc server.Client) *proposalsEndpoint {
-	return &proposalsEndpoint{mc}
+    return &proposalsEndpoint{mc}
 }
 
 func (pe *proposalsEndpoint) List(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
 
-	nodeId := req.URL.Query().Get("nodeid")
-	proposals, err := pe.mysteriumClient.FindProposals(nodeId)
-	if err != nil {
-		utils.SendError(resp, err, http.StatusInternalServerError)
-		return
-	}
-	proposalsDto := proposalsDto{proposals}
-	utils.WriteAsJson(proposalsDto, resp)
+    nodeId := req.URL.Query().Get("provider_id")
+    proposals, err := pe.mysteriumClient.FindProposals(nodeId)
+    if err != nil {
+        utils.SendError(resp, err, http.StatusInternalServerError)
+        return
+    }
+    proposalsDto := proposalsDto{mapProposalsToTeqDto(proposals, proposalToDto)}
+    utils.WriteAsJson(proposalsDto, resp)
 }
 
 func AddRoutesForProposals(router *httprouter.Router, mc server.Client) {
-	pe := NewProposalsEndpoint(mc)
-	router.GET("/proposals", pe.List)
+    pe := NewProposalsEndpoint(mc)
+    router.GET("/proposals", pe.List)
 }
