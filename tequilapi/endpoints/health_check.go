ac5f50932d47b0e89af83a445957436581681dd9
Tadas V <tadovas@gmail.com>
2018-01-04 10:41:53 +0200

    Report process id of client in healthcheck endpoint

--- tequilapi/endpoints/health_check.go
+++ tequilapi/endpoints/health_check.go
@@ -8,26 +8,29 @@ import (
 )
 
 type healthCheckData struct {
-	Uptime string `json:"uptime"`
+	Uptime  string `json:"uptime"`
+	Process int    `json:"process"`
 }
 
 type healthCheckEndpoint struct {
 	startTime       time.Time
 	currentTimeFunc func() time.Time
+	processNumber   int
 }
 
 /*
 HealthCheckEndpointFactory creates a structure with single HealthCheck method for healthcheck serving as http,
 currentTimeFunc is injected for easier testing
 */
-func HealthCheckEndpointFactory(currentTimeFunc func() time.Time) *healthCheckEndpoint {
+func HealthCheckEndpointFactory(currentTimeFunc func() time.Time, procId func() int) *healthCheckEndpoint {
 	startTime := currentTimeFunc()
-	return &healthCheckEndpoint{startTime, currentTimeFunc}
+	return &healthCheckEndpoint{startTime, currentTimeFunc, procId()}
 }
 
 func (hce *healthCheckEndpoint) HealthCheck(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 	status := healthCheckData{
-		Uptime: hce.currentTimeFunc().Sub(hce.startTime).String(),
+		Uptime:  hce.currentTimeFunc().Sub(hce.startTime).String(),
+		Process: hce.processNumber,
 	}
 	utils.WriteAsJson(status, writer)
 }
--- tequilapi/http_routes.go
+++ tequilapi/http_routes.go
@@ -3,6 +3,7 @@ package tequilapi
 import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/mysterium/node/tequilapi/endpoints"
+	"os"
 	"time"
 )
 
@@ -10,7 +11,7 @@ func NewApiRouter() *httprouter.Router {
 	router := httprouter.New()
 	router.HandleMethodNotAllowed = true
 
-	router.GET("/healthcheck", endpoints.HealthCheckEndpointFactory(time.Now).HealthCheck)
+	router.GET("/healthcheck", endpoints.HealthCheckEndpointFactory(time.Now, os.Getpid).HealthCheck)
 
 	return router
 }
