c3f2cce9f70d85b4864f94be657d6e0663c2bc8d
Waldz <valdas@mysterium.network>
2018-01-07 20:46:36 +0200

    Verifier got possibility to deny unauthorized messages

--- identity/verifier.go
+++ identity/verifier.go
@@ -2,31 +2,52 @@ package identity
 
 import (
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/syndtr/goleveldb/leveldb/errors"
 )
 
 type Verifier interface {
 	Verify(message []byte, signature Signature) bool
 }
 
-type ethereumVerifier struct {
+func NewVerifyIsAuthorized() *verifyIsAuthorized {
+	return &verifyIsAuthorized{}
+}
+
+func NewVerifyIdentity(peerIdentity Identity) *verifyIdentity {
+	return &verifyIdentity{peerIdentity}
+}
+
+type verifyIsAuthorized struct{}
+
+func (ev *verifyIsAuthorized) Verify(message []byte, signature Signature) bool {
+	_, err := extractSignerIdentity(message, signature)
+	return err == nil
+}
+
+type verifyIdentity struct {
 	peerIdentity Identity
 }
 
-func NewVerifier(peerIdentity Identity) *ethereumVerifier {
-	return &ethereumVerifier{peerIdentity}
+func (ev *verifyIdentity) Verify(message []byte, signature Signature) bool {
+	identity, err := extractSignerIdentity(message, signature)
+	if err != nil {
+		return false
+	}
+
+	return identity == ev.peerIdentity
 }
 
-func (ev *ethereumVerifier) Verify(message []byte, signature Signature) bool {
+func extractSignerIdentity(message []byte, signature Signature) (Identity, error) {
 	signatureBytes := signature.Bytes()
 	if len(signatureBytes) == 0 {
-		return false
+		return Identity{}, errors.New("Signature is empty")
 	}
 
 	recoveredKey, err := crypto.Ecrecover(messageHash(message), signatureBytes)
 	if err != nil {
-		return false
+		return Identity{}, err
 	}
 	recoveredAddress := crypto.PubkeyToAddress(*crypto.ToECDSAPub(recoveredKey)).Hex()
 
-	return FromAddress(recoveredAddress) == ev.peerIdentity
+	return FromAddress(recoveredAddress), nil
 }
--- identity/verifier_test.go
+++ identity/verifier_test.go
@@ -5,42 +5,86 @@ import (
 	"testing"
 )
 
-func TestVerifier_VerifyWhenSignatureIsCorrect(t *testing.T) {
+func TestVerifier_extractSignerWhenSignatureIsCorrect(t *testing.T) {
 	message := []byte("Boop!")
 	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
 
-	verifier := NewVerifier(FromAddress("0x53a835143c0ef3bbcbfa796d7eb738ca7dd28f68"))
-	assert.True(t, verifier.Verify(message, signature))
+	signerId, err := extractSignerIdentity(message, signature)
+	assert.NoError(t, err)
+	assert.Exactly(t, FromAddress("0x53a835143c0ef3bbcbfa796d7eb738ca7dd28f68"), signerId)
 }
 
-func TestVerifier_VerifyUppercasedIdentity(t *testing.T) {
-	message := []byte("Boop!")
-	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
-
-	verifier := NewVerifier(FromAddress("0x53A835143C0EF3BBCBFA796d7eb738CA7DD28F68"))
-	assert.True(t, verifier.Verify(message, signature))
-}
-
-func TestVerifyFunctionReturnsFalseWhenSignatureIsEmpty(t *testing.T) {
+func TestVerifier_extractSignerWhenSignatureIsEmpty(t *testing.T) {
 	message := []byte("boop!")
 	signature := SignatureHex("")
 
-	verifier := NewVerifier(FromAddress("0x53A835143C0EF3BBCBFA796d7eb738CA7DD28F68"))
-	assert.False(t, verifier.Verify(message, signature))
+	signerId, err := extractSignerIdentity(message, signature)
+	assert.EqualError(t, err, "Signature is empty")
+	assert.Exactly(t, Identity{}, signerId)
 }
 
-func TestVerifyFunctionReturnsFalseWhenSignatureIsMalformed(t *testing.T) {
+func TestVerifier_extractSignerWhenSignatureIsMalformed(t *testing.T) {
 	message := []byte("boop!")
-	signature := SignatureHex("malformed")
+	signature := SignatureHex("7369676e6564")
 
-	verifier := NewVerifier(FromAddress("0x53A835143C0EF3BBCBFA796d7eb738CA7DD28F68"))
-	assert.False(t, verifier.Verify(message, signature))
+	signerId, err := extractSignerIdentity(message, signature)
+	assert.EqualError(t, err, "invalid signature length")
+	assert.Exactly(t, Identity{}, signerId)
 }
 
-func TestVerifyFunctionReturnsFalseWhenMessageIsChanged(t *testing.T) {
+func TestVerifier_extractSignerWhenMessageIsChanged(t *testing.T) {
 	message := []byte("boop!")
 	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
 
-	verifier := NewVerifier(FromAddress("0x53A835143C0EF3BBCBFA796d7eb738CA7DD28F68"))
+	signerId, err := extractSignerIdentity(message, signature)
+	assert.NoError(t, err)
+	assert.Exactly(t, FromAddress("0xb1fe295162488f618201b92672e3299436b80d9e"), signerId)
+}
+
+func TestVerifier_Identity_Verify(t *testing.T) {
+	message := []byte("Boop!")
+	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
+
+	verifier := NewVerifyIdentity(FromAddress("0x53a835143c0ef3bbcbfa796d7eb738ca7dd28f68"))
+	assert.True(t, verifier.Verify(message, signature))
+}
+
+func TestVerifier_Identity_VerifyWithUppercaseIdentity(t *testing.T) {
+	message := []byte("Boop!")
+	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
+
+	verifier := NewVerifyIdentity(FromAddress("0x53A835143C0EF3BBCBFA796d7eb738CA7DD28F68"))
+	assert.True(t, verifier.Verify(message, signature))
+}
+
+func TestVerifier_Identity_VerifyWhenWrongSender(t *testing.T) {
+	message := []byte("boop!")
+	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
+
+	verifier := NewVerifyIdentity(FromAddress("0x53a835143c0ef3bbcbfa796d7eb738ca7dd28f68"))
 	assert.False(t, verifier.Verify(message, signature))
+}
+
+func TestVerifier_IsAuthorized_Verify(t *testing.T) {
+	message := []byte("Boop!")
+	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
+
+	verifier := NewVerifyIsAuthorized()
+	assert.True(t, verifier.Verify(message, signature))
+}
+
+func TestVerifier_IsAuthorized_VerifyWhenMalformedSignature(t *testing.T) {
+	message := []byte("Boop!")
+	signature := SignatureHex("7369676e6564")
+
+	verifier := NewVerifyIsAuthorized()
+	assert.False(t, verifier.Verify(message, signature))
+}
+
+func TestVerifier_IsAuthorized_VerifyWhenWrongSender(t *testing.T) {
+	message := []byte("boop!")
+	signature := SignatureHex("1f89542f406b2d638fe09cd9912d0b8c0b5ebb4aef67d52ab046973e34fb430a1953576cd19d140eddb099aea34b2985fbd99e716d3b2f96a964141fdb84b32000")
+
+	verifier := NewVerifyIsAuthorized()
+	assert.True(t, verifier.Verify(message, signature))
 }
