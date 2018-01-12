f00143088a2fd6432f2ef412e5af9ff0e8f46a7c
Antanas Masevicius <antanas@mysterium.network>
2018-01-12 11:01:54 +0200

    hidentity.verifier_fake uses signature.EqualsTo now

--- identity/verifier_fake.go
+++ identity/verifier_fake.go
@@ -1,10 +1,8 @@
 package identity
 
-import "bytes"
-
 type VerifierFake struct{}
 
 func (verifier *VerifierFake) Verify(message []byte, signature Signature) bool {
 	signatureExpected := messageFakeHash(message)
-	return bytes.Equal(signature.raw, signatureExpected)
+	return signature.EqualsTo(SignatureBytes(signatureExpected))
 }
