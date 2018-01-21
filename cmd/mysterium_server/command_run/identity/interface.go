c8e4c60a02a5b47a67b6023af0aebc67a7975676
Donatas Kucinskas <donce.lt@gmail.com>
2018-01-21 10:18:04 +0200

    Fix linter errors in "command_run.identity" and "location"

--- cmd/mysterium_server/command_run/identity/interface.go
+++ cmd/mysterium_server/command_run/identity/interface.go
@@ -2,6 +2,7 @@ package identity
 
 import "github.com/mysterium/node/identity"
 
+// HandlerInterface allows selecting identity to be used
 type HandlerInterface interface {
 	UseExisting(address string) (identity.Identity, error)
 	UseLast() (identity.Identity, error)
