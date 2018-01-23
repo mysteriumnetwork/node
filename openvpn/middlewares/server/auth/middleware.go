f961ffe38a476572d8c538f0caeff25734a68092
Antanas Masevicius <antanas@mysterium.network>
2018-01-23 11:14:30 +0200

    PR remarks fixed; ConsumeLine has been splitted

--- openvpn/middlewares/server/auth/middleware.go
+++ openvpn/middlewares/server/auth/middleware.go
@@ -15,8 +15,8 @@ type middleware struct {
 	connection    net.Conn
 	username      string
 	password      string
-	clientId      int
-	keyId         int
+	clientID      int
+	keyID         int
 	state         middlewares.State
 }
 
@@ -55,8 +55,8 @@ func (m *middleware) ConsumeLine(line string) (consumed bool, err error) {
 	if len(match) > 0 {
 		m.Reset()
 		m.state = middlewares.STATE_AUTH
-		m.clientId, err = strconv.Atoi(match[1])
-		m.keyId, err = strconv.Atoi(match[2])
+		m.clientID, err = strconv.Atoi(match[1])
+		m.keyID, err = strconv.Atoi(match[2])
 		return true, nil
 	}
 
@@ -69,8 +69,8 @@ func (m *middleware) ConsumeLine(line string) (consumed bool, err error) {
 	if len(match) > 0 {
 		m.Reset()
 		m.state = middlewares.STATE_AUTH
-		m.clientId, err = strconv.Atoi(match[1])
-		m.keyId, err = strconv.Atoi(match[2])
+		m.clientID, err = strconv.Atoi(match[1])
+		m.keyID, err = strconv.Atoi(match[2])
 		return true, nil
 	}
 
@@ -86,7 +86,7 @@ func (m *middleware) ConsumeLine(line string) (consumed bool, err error) {
 
 	match = rule.FindStringSubmatch(line)
 	if len(match) > 0 {
-		if m.clientId < 0 {
+		if m.clientID < 0 {
 			return false, fmt.Errorf("wrong auth state, no client id")
 		}
 		m.password = match[1]
@@ -100,7 +100,7 @@ func (m *middleware) ConsumeLine(line string) (consumed bool, err error) {
 
 	match = rule.FindStringSubmatch(line)
 	if len(match) > 0 {
-		if m.clientId < 0 {
+		if m.clientID < 0 {
 			return false, fmt.Errorf("wrong auth state, no client id")
 		}
 		m.username = match[1]
@@ -114,40 +114,45 @@ func (m *middleware) ConsumeLine(line string) (consumed bool, err error) {
 
 	match = rule.FindStringSubmatch(line)
 	if len(match) > 0 {
-		defer m.Reset()
-
-		if m.username == "" || m.password == "" {
-			return false, fmt.Errorf("missing username or password")
-		}
-
-		log.Info("authenticating user: ", m.username, " clientId: ", m.clientId, " keyId: ", m.keyId)
-
-		authenticated, err := m.authenticator(m.username, m.password)
-		if err != nil {
-			return false, err
-		}
-
-		if authenticated {
-			_, err = m.connection.Write([]byte("client-auth-nt " + strconv.Itoa(m.clientId) + " " + strconv.Itoa(m.keyId) + "\n"))
-			if err != nil {
-				return false, err
-			}
-		} else {
-			_, err = m.connection.Write([]byte("client-deny " + strconv.Itoa(m.clientId) + " " + strconv.Itoa(m.keyId) +
-				" wrong username or password \n"))
-			if err != nil {
-				return false, err
-			}
-		}
-		return true, nil
+		return m.authenticateClient()
 	}
 
 	return false, nil
 }
+
+func (m *middleware) authenticateClient() (consumed bool, err error) {
+	defer m.Reset()
+
+	if m.username == "" || m.password == "" {
+		return false, fmt.Errorf("missing username or password")
+	}
+
+	log.Info("authenticating user: ", m.username, " clientID: ", m.clientID, " keyID: ", m.keyID)
+
+	authenticated, err := m.authenticator(m.username, m.password)
+	if err != nil {
+		return false, err
+	}
+
+	if authenticated {
+		_, err = m.connection.Write([]byte("client-auth-nt " + strconv.Itoa(m.clientID) + " " + strconv.Itoa(m.keyID) + "\n"))
+		if err != nil {
+			return false, err
+		}
+	} else {
+		_, err = m.connection.Write([]byte("client-deny " + strconv.Itoa(m.clientID) + " " + strconv.Itoa(m.keyID) +
+			" wrong username or password \n"))
+		if err != nil {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
 func (m *middleware) Reset() {
 	m.username = ""
 	m.password = ""
-	m.clientId = -1
-	m.keyId = -1
+	m.clientID = -1
+	m.keyID = -1
 	m.state = middlewares.STATE_UNDEFINED
 }
