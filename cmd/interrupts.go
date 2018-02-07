321b75bb3c7ef4107b7cbfd4153389c2a9155a6b
Donatas Kucinskas <donce.lt@gmail.com>
2018-02-07 11:57:59 +0200

    Rename Stopper to ApplicationStopper

--- cmd/interrupts.go
+++ cmd/interrupts.go
@@ -6,18 +6,18 @@ import (
 	"syscall"
 )
 
-// Stopper stops application and performs required cleanup tasks
-type Stopper func()
+// ApplicationStopper stops application and performs required cleanup tasks
+type ApplicationStopper func()
 
 // StopOnInterrupts invokes given stopper on SIGTERM and SIGHUP interrupts
-func StopOnInterrupts(stop Stopper) {
+func StopOnInterrupts(stop ApplicationStopper) {
 	sigterm := make(chan os.Signal)
 	signal.Notify(sigterm, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
 
 	go waitTerminationSignal(sigterm, stop)
 }
 
-func waitTerminationSignal(termination chan os.Signal, stop Stopper) {
+func waitTerminationSignal(termination chan os.Signal, stop ApplicationStopper) {
 	<-termination
 	stop()
 }
--- tequilapi/endpoints/stop.go
+++ tequilapi/endpoints/stop.go
@@ -7,15 +7,15 @@ import (
 	"time"
 )
 
-// Stopper stops application and performs required cleanup tasks
-type Stopper func()
+// ApplicationStopper stops application and performs required cleanup tasks
+type ApplicationStopper func()
 
 // AddRouteForStop adds stop route to given router
-func AddRouteForStop(router *httprouter.Router, stop Stopper, delay time.Duration) {
+func AddRouteForStop(router *httprouter.Router, stop ApplicationStopper, delay time.Duration) {
 	router.POST("/stop", newStopHandler(stop, delay))
 }
 
-func newStopHandler(stop Stopper, delay time.Duration) httprouter.Handle {
+func newStopHandler(stop ApplicationStopper, delay time.Duration) httprouter.Handle {
 	return func(response http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 		log.Info("Client is stopping")
 		delayExecution(stop, delay)
