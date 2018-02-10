64fa2533d0449301aa04a7c646b01d36a693fb07
Waldz <valdas@mysterium.network>
2018-02-10 12:38:16 +0200

    Inject Tequilapi port environment variable

--- bin/client_docker/docker-entrypoint.sh
+++ bin/client_docker/docker-entrypoint.sh
@@ -8,4 +8,6 @@ OS_DIR_RUN="/var/run/mysterium-client"
 exec /usr/bin/mysterium_client \
     --config-dir=$OS_DIR_CONFIG \
     --data-dir=$OS_DIR_DATA \
-    --runtime-dir=$OS_DIR_RUN
+    --runtime-dir=$OS_DIR_RUN \
+    --tequilapi.address=localhost \
+    --tequilapi.port=$MYSTERIUM_CLIENT_TEQUILAPI_PORT
