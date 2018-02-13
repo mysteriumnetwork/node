55eb1d82f2f6f69d8b4ffd7a4b9a0fb414eb1ee1
Tadas V <tadovas@gmail.com>
2018-02-13 09:59:07 +0200

    Add signal handler for mysterium service to stop gracefully

--- cmd/mysterium_server/mysterium_server.go
+++ cmd/mysterium_server/mysterium_server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/mysterium/node/cmd"
 	"github.com/mysterium/node/cmd/commands/server"
 	"os"
 )
@@ -13,14 +14,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	cmd := server.NewCommand(options)
+	serverCommand := server.NewCommand(options)
 
-	if err := cmd.Start(); err != nil {
+	if err := serverCommand.Start(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	if err = cmd.Wait(); err != nil {
+	cmd.StopOnInterrupts(cmd.NewApplicationStopper(serverCommand.Kill))
+
+	if err = serverCommand.Wait(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
