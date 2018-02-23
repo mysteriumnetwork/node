fa01d008cb8932765dde3f934b6a7b51d7835fb5
Tadas V <tadovas@gmail.com>
2018-02-23 14:24:14 +0200

    Fix too deep directory archiving

--- bin/travis_scripts/package_tar_gz.sh
+++ bin/travis_scripts/package_tar_gz.sh
@@ -5,10 +5,13 @@ set -e
 KIND=$1 #client or server
 ARCHIVE_NAME=$2 # i.e. output/mysterium_server.tar.gz
 
-ARCHIVE_DIR=`dirname $ARCHIVE_NAME`/mysterium_${KIND}
-mkdir -p $ARCHIVE_DIR
+ARCHIVE_ROOT=`dirname $ARCHIVE_NAME`
+ARCHIVE_DIR="mysterium_${KIND}"
+FULL_ARCHIVE_DIR="$ARCHIVE_ROOT/$ARCHIVE_DIR"
+mkdir -p "$FULL_ARCHIVE_DIR"
 
-cp "build/$KIND/mysterium_$KIND" "$ARCHIVE_DIR"
-cp -r "bin/${KIND}_package/config" "$ARCHIVE_DIR"
+cp "build/$KIND/mysterium_$KIND" "$FULL_ARCHIVE_DIR"
+cp -r "bin/${KIND}_package/config" "$FULL_ARCHIVE_DIR"
 
-tar -zcvf $ARCHIVE_NAME $ARCHIVE_DIR && rm -rf $ARCHIVE_DIR
+tar -zcvf "$ARCHIVE_NAME" -C "$ARCHIVE_ROOT" "$ARCHIVE_DIR"
+rm -rf "$ARCHIVE_DIR"
