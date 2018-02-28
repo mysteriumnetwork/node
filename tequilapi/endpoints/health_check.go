4da6c89690cdf23490e547480c3b26565fe797fa
Tadas V <tadovas@gmail.com>
2018-02-28 16:46:02 +0200

    Version fields exposes build version on tequila api

--- tequilapi/endpoints/health_check.go
+++ tequilapi/endpoints/health_check.go
@@ -10,27 +10,35 @@ import (
 type healthCheckData struct {
 	Uptime  string `json:"uptime"`
 	Process int    `json:"process"`
+	Version string `json:"version"`
 }
 
 type healthCheckEndpoint struct {
 	startTime       time.Time
 	currentTimeFunc func() time.Time
 	processNumber   int
+	buildVersion    string
 }
 
 /*
 HealthCheckEndpointFactory creates a structure with single HealthCheck method for healthcheck serving as http,
 currentTimeFunc is injected for easier testing
 */
-func HealthCheckEndpointFactory(currentTimeFunc func() time.Time, procID func() int) *healthCheckEndpoint {
+func HealthCheckEndpointFactory(currentTimeFunc func() time.Time, procID func() int, buildVersion string) *healthCheckEndpoint {
 	startTime := currentTimeFunc()
-	return &healthCheckEndpoint{startTime, currentTimeFunc, procID()}
+	return &healthCheckEndpoint{
+		startTime,
+		currentTimeFunc,
+		procID(),
+		buildVersion,
+	}
 }
 
 func (hce *healthCheckEndpoint) HealthCheck(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 	status := healthCheckData{
 		Uptime:  hce.currentTimeFunc().Sub(hce.startTime).String(),
 		Process: hce.processNumber,
+		Version: hce.buildVersion,
 	}
 	utils.WriteAsJSON(status, writer)
 }
--- tequilapi/endpoints/health_check_test.go
+++ tequilapi/endpoints/health_check_test.go
@@ -16,14 +16,19 @@ func TestHealthCheckReturnsExpectedJsonObject(t *testing.T) {
 	tick1 := time.Unix(0, 0)
 	tick2 := tick1.Add(time.Minute)
 
-	handlerFunc := HealthCheckEndpointFactory(newMockTimer([]time.Time{tick1, tick2}).Now, func() int { return 1 }).HealthCheck
+	handlerFunc := HealthCheckEndpointFactory(
+		newMockTimer([]time.Time{tick1, tick2}).Now,
+		func() int { return 1 },
+		"version_string",
+	).HealthCheck
 	handlerFunc(resp, req, httprouter.Params{})
 
 	assert.JSONEq(
 		t,
 		`{
 			"uptime" : "1m0s",
-			"process" : 1
+			"process" : 1,
+			"version" : "version_string"
 		}`,
 		resp.Body.String())
 }
