21400fd45210e3ce0f9b9ccdda78c9578eac3702
Tadas V <tadovas@gmail.com>
2018-03-06 13:03:09 +0200

    Removed outputs

--- bin/travis_scripts/ensure_docker.sh
+++ bin/travis_scripts/ensure_docker.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
-echo ***** Docker info *****
+
 docker info
-echo ***** Docker info end**
--- bin/travis_scripts/ensure_docker_compose.sh
+++ bin/travis_scripts/ensure_docker_compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-echo ***** Docker compose *****
+
 docker-compose version
-echo ***** Docker compose end**
+
