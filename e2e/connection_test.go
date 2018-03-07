33aec1095397c24a4c4a4e2d45ffc13acd6d9b90
Tadas V <tadovas@gmail.com>
2018-03-07 14:40:14 +0200

    e2e package for go e2e tests, separate docker-compose file

---
+++ e2e/connection_test.go
@@ -0,0 +1,21 @@
+package e2e
+
+import (
+	"flag"
+	"github.com/mysterium/node/tequilapi/client"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var host = flag.String("tequila.host", "localhost", "Specify tequila host for e2e tests")
+var port = flag.Int("tequila.port", 4050, "Specify tequila port for e2e tests")
+
+func TestClientConnectsToNode(t *testing.T) {
+	//we cannot move tequilApi as var outside - host and port are not initialized yet :(
+	//something to do, when "go test" calls flag.Parse()
+	tequilApi := client.NewClient(*host, *port)
+
+	status, err := tequilApi.Status()
+	assert.NoError(t, err)
+	assert.Equal(t, "NotConnected", status.Status)
+}
