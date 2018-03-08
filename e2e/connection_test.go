5c1bda8408f58daf632fefbf9957fca2ac5b273c
Tadas V <tadovas@gmail.com>
2018-03-08 15:56:43 +0200

    Rename test to reflect it's testing case

--- e2e/connection_test.go
+++ e2e/connection_test.go
@@ -10,7 +10,7 @@ import (
 var host = flag.String("tequila.host", "localhost", "Specify tequila host for e2e tests")
 var port = flag.Int("tequila.port", 4050, "Specify tequila port for e2e tests")
 
-func TestClientConnectsToNode(t *testing.T) {
+func TestClientStatusIsNotConnectedOnStartup(t *testing.T) {
 	//we cannot move tequilApi as var outside - host and port are not initialized yet :(
 	//something related to how "go test" calls flag.Parse()
 	tequilApi := client.NewClient(*host, *port)
