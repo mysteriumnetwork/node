70b4efb97fc4a37644400201a68ed7653a47f30f
Tadas V <tadovas@gmail.com>
2018-03-08 18:18:09 +0200

    client connection test implemented, api and db services added

--- e2e/connection_test.go
+++ e2e/connection_test.go
@@ -2,9 +2,11 @@ package e2e
 
 import (
 	"flag"
+	"github.com/cihub/seelog"
 	"github.com/mysterium/node/tequilapi/client"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 var host = flag.String("tequila.host", "localhost", "Specify tequila host for e2e tests")
@@ -18,4 +20,50 @@ func TestClientStatusIsNotConnectedOnStartup(t *testing.T) {
 	status, err := tequilApi.Status()
 	assert.NoError(t, err)
 	assert.Equal(t, "NotConnected", status.Status)
+
+	identities, err := tequilApi.GetIdentities()
+	assert.NoError(t, err)
+
+	var identity client.IdentityDTO
+	if len(identities) < 1 {
+		identity, err = tequilApi.NewIdentity("")
+		assert.NoError(t, err)
+	} else {
+		identity = identities[0]
+	}
+	seelog.Info("Client identity is: ", identity.Address)
+
+	err = tequilApi.Unlock(identity.Address, "")
+	assert.NoError(t, err)
+
+	proposals, err := tequilApi.Proposals()
+	if err != nil {
+		assert.Error(t, err)
+		assert.FailNow(t, "Proposals returned error - no point to continue")
+	}
+
+	//expect exactly one proposal
+	if len(proposals) != 1 {
+		assert.FailNow(t, "Exactly one proposal is expected - something is not right!")
+	}
+
+	proposal := proposals[0]
+	seelog.Info("Connecting to provider: ", proposal)
+
+	_, err = tequilApi.Connect(identity.Address, proposal.ProviderID)
+	assert.NoError(t, err)
+
+	time.Sleep(10 * time.Second)
+	status, err = tequilApi.Status()
+	assert.NoError(t, err)
+	assert.Equal(t, "Connected", status.Status)
+
+	err = tequilApi.Disconnect()
+	assert.NoError(t, err)
+
+	time.Sleep(10 * time.Second)
+	status, err = tequilApi.Status()
+	assert.NoError(t, err)
+	assert.Equal(t, "NotConnected", status.Status)
+
 }
