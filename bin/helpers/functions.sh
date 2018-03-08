09a2ca89fba8cf0cd3c001491a36b1d4c9a8a44f
Tadas V <tadovas@gmail.com>
2018-03-08 09:11:10 +0200

    Docker ignores .git repo and build folder, building script doesnt depend on git

--- bin/helpers/functions.sh
+++ bin/helpers/functions.sh
@@ -6,6 +6,6 @@ function get_linker_ldflags {
     echo -n "-X 'github.com/mysterium/node/cmd/commands/server.natsServerIP=${NATS_SERVER_IP}' "
     [ -n "$BRANCH" ] && echo -n "-X 'github.com/mysterium/node/version.gitBranch=${BRANCH}' "
     [ -n "$COMMIT" ] && echo -n "-X 'github.com/mysterium/node/version.gitCommit=${COMMIT}' "
-    [ -n "$TRAVIS_JOB_NUMBER" ] && echo -n "-X 'github.com/mysterium/node/version.buildNumber=${TRAVIS_JOB_NUMBER}' "
+    [ -n "$BUILD_NUMBER" ] && echo -n "-X 'github.com/mysterium/node/version.buildNumber=${BUILD_NUMBER}' "
 
 }
