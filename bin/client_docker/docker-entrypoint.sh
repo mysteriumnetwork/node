3c836f98eb66adec9d6800f060ad7a8d91d149e4
Tadas V <tadovas@gmail.com>
2018-03-13 08:17:35 +0200

    Fixed alpine docker image, linux dns script noop

--- bin/client_docker/docker-entrypoint.sh
+++ bin/client_docker/docker-entrypoint.sh
@@ -5,6 +5,8 @@ OS_DIR_CONFIG="/etc/mysterium-client"
 OS_DIR_DATA="/var/lib/mysterium-client"
 OS_DIR_RUN="/var/run/mysterium-client"
 
+mkdir -p $OS_DIR_RUN
+
 exec /usr/bin/mysterium_client \
     --config-dir=$OS_DIR_CONFIG \
     --data-dir=$OS_DIR_DATA \
