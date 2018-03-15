bd7652b47c32aef00e60c783606f1c8f13b9418b
interro <interro@gmail.com>
2018-03-15 16:26:13 +0200

    ClientCountry -> ConsumerCountry

--- server/dto/session_stats.go
+++ server/dto/session_stats.go
@@ -4,5 +4,5 @@ type SessionStats struct {
 	BytesSent     int    `json:"bytes_sent"`
 	BytesReceived int    `json:"bytes_received"`
 	ProviderID    string `json:"provider_id"`
-	ClientCountry string `json:"client_country"`
+	ConsumerCountry string `json:"consumer_country"`
 }
