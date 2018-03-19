686558a9add8c638f9f7ed791cd33c28abc9f89d
Antanas Masevicius <antanas@mysterium.network>
2018-03-19 19:08:45 +0200

    removed privileged and mknod cap; docker entrypoints reworked

--- bin/client_docker/docker-entrypoint.sh
+++ bin/client_docker/docker-entrypoint.sh
@@ -5,6 +5,13 @@ OS_DIR_CONFIG="/etc/mysterium-client"
 OS_DIR_DATA="/var/lib/mysterium-client"
 OS_DIR_RUN="/var/run/mysterium-client"
 
+if [ ! -d /dev/net ]; then
+    mkdir -p /dev/net
+fi
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
+
 if [ ! -d "$OS_DIR_RUN" ]; then
     mkdir -p $OS_DIR_RUN
 fi
