a488c3dd07be79e69e5917bf6777dba9412c2634
Waldz <valdas@mysterium.network>
2018-06-13 10:32:20 +0300

    Dont eat linter messages in uncleaned packages
    Signed-off-by: Waldz <valdas@mysterium.network>

--- bin/helpers/output.sh
+++ bin/helpers/output.sh
@@ -3,13 +3,18 @@
 # Colorful output
 
 SUCCESS_COLOR='\033[0;32m' # green
+WARNING_COLOR='\033[0;33m' # yellow
 FAILURE_COLOR="\033[0;31m" # red
 DEFAULT_COLOR="\033[0m"
 
 print_success () {
-    echo -e $SUCCESS_COLOR$1$DEFAULT_COLOR
+    echo -e "${SUCCESS_COLOR}$1${DEFAULT_COLOR}"
+}
+
+print_warning () {
+    echo -e "$WARNING_COLOR$1${DEFAULT_COLOR}"
 }
 
 print_error () {
-    echo -e $FAILURE_COLOR$1$DEFAULT_COLOR
+    echo -e "${FAILURE_COLOR}$1${DEFAULT_COLOR}"
 }
