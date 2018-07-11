c2c2951d335da17b99634fe96679f93f3093fa11
Waldz <valdas@mysterium.network>
2018-07-11 13:47:25 +0300

    Inject semantic version during CI building
    Signed-off-by: Waldz <valdas@mysterium.network>

--- bin/helpers/functions.sh
+++ bin/helpers/functions.sh
@@ -2,8 +2,8 @@
 
 # Map environment variables to flags for Golang linker's -ldflags usage
 function get_linker_ldflags {
-    [ -n "$BRANCH" ] && echo -n "-X 'github.com/mysterium/node/metadata.BuildBranch=${BRANCH}' "
-    [ -n "$COMMIT" ] && echo -n "-X 'github.com/mysterium/node/metadata.BuildCommit=${COMMIT}' "
+    [ -n "$BUILD_BRANCH" ] && echo -n "-X 'github.com/mysterium/node/metadata.BuildBranch=${BUILD_BRANCH}' "
+    [ -n "$BUILD_COMMIT" ] && echo -n "-X 'github.com/mysterium/node/metadata.BuildCommit=${BUILD_COMMIT}' "
     [ -n "$BUILD_NUMBER" ] && echo -n "-X 'github.com/mysterium/node/metadata.BuildNumber=${BUILD_NUMBER}' "
-
+    [ -n "$BUILD_VERSION" ] && echo -n "-X 'github.com/mysterium/node/metadata.Version=${BUILD_VERSION}' "
 }
