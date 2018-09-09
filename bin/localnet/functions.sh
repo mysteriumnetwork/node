7350b637989e75a68a5fc3ca51178fd433c6bca3
Waldz <valdas@mysterium.network>
2018-09-09 19:12:24 +0300

    Rename localnet container "local-node" -> to "geth"

--- bin/localnet/functions.sh
+++ bin/localnet/functions.sh
@@ -44,7 +44,7 @@ setup () {
         exit 1
     fi
 
-    ${dockerComposeCmd} run local-node init genesis.json
+    ${dockerComposeCmd} run geth init genesis.json
     if [ ! $? -eq 0 ]; then
         print_error "Geth node initialization failed"
         cleanup "$@"
@@ -52,7 +52,7 @@ setup () {
     fi
 
 
-    ${dockerComposeCmd} up -d broker discovery local-node
+    ${dockerComposeCmd} up -d broker discovery geth
     if [ ! $? -eq 0 ]; then
         print_error "Error starting other services"
         cleanup "$@"
@@ -66,7 +66,7 @@ setup () {
         --keystore.directory=bin/localnet/account \
         --ether.address=0xa754f0d31411d88e46aed455fa79b9fced122497 \
         --ether.passphrase `cat bin/localnet/local_acc_password.txt` \
-        --geth.url=http://local-node:8545
+        --geth.url=http://geth:8545
     if [ ! $? -eq 0 ]; then
         print_error "Error deploying contracts"
         cleanup "$@"
