a06315d1cec97f94e50729f68d08f76a5bee1146
Waldz <valdas@mysterium.network>
2018-10-12 14:16:03 +0300

    Expose connection manager

--- cmd/di.go
+++ cmd/di.go
@@ -70,7 +70,8 @@ type Dependencies struct {
 	IPResolver       ip.Resolver
 	LocationResolver location.Resolver
 
-	ServiceManager *service.Manager
+	ConnectionManager connection.Manager
+	ServiceManager    *service.Manager
 }
 
 // Bootstrap initiates all container dependencies
@@ -156,20 +157,20 @@ func (di *Dependencies) bootstrapNodeComponents(nodeOptions node.Options) {
 		di.SignerFactory,
 	)
 
-	connectionManager := connection.NewManager(di.MysteriumClient, dialogFactory, promiseIssuerFactory, connectionFactory)
+	di.ConnectionManager = connection.NewManager(di.MysteriumClient, dialogFactory, promiseIssuerFactory, connectionFactory)
 
 	router := tequilapi.NewAPIRouter()
 	tequilapi_endpoints.AddRouteForStop(router, utils.SoftKiller(di.Shutdown))
 	tequilapi_endpoints.AddRoutesForIdentities(router, di.IdentityManager, di.MysteriumClient, di.SignerFactory)
-	tequilapi_endpoints.AddRoutesForConnection(router, connectionManager, di.IPResolver, statsKeeper)
-	tequilapi_endpoints.AddRoutesForLocation(router, connectionManager, locationDetector, originalLocationCache)
+	tequilapi_endpoints.AddRoutesForConnection(router, di.ConnectionManager, di.IPResolver, statsKeeper)
+	tequilapi_endpoints.AddRoutesForLocation(router, di.ConnectionManager, locationDetector, originalLocationCache)
 	tequilapi_endpoints.AddRoutesForProposals(router, di.MysteriumClient)
 	identity_registry.AddIdentityRegistrationEndpoint(router, di.IdentityRegistration, di.IdentityRegistry)
 
 	httpAPIServer := tequilapi.NewServer(nodeOptions.TequilapiAddress, nodeOptions.TequilapiPort, router)
 
 	di.NodeOptions = nodeOptions
-	di.Node = node.NewNode(connectionManager, httpAPIServer, originalLocationCache)
+	di.Node = node.NewNode(di.ConnectionManager, httpAPIServer, originalLocationCache)
 }
 
 // BootstrapServiceComponents initiates ServiceManager dependency
