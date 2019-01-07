bca5dbba2ca55a071cfa7cfb26aa55e1dd4aa4a2
Dmitry Shihovtsev <soffokulus@gmail.com>
2019-01-07 04:23:23 +0100

    Better error handling for non JSON responses. #535

--- market/mysterium/response.go
+++ market/mysterium/response.go
@@ -24,22 +24,20 @@ import (
 	"net/http"
 )
 
-type errorResponse struct {
-	Error string `json:"error"`
-}
-
 // ParseResponseError checks the respose for correctness and return error if it is invalid
-func ParseResponseError(response *http.Response) error {
+func ParseResponseError(response *http.Response) (err error) {
 	if response.StatusCode < 200 || response.StatusCode >= 300 {
-		var error errorResponse
+		var errorMsg []byte
 
-		err := ParseResponseJSON(response, &error)
-		if err != nil {
-			return err
+		if response.Body != nil {
+			errorMsg, err = ioutil.ReadAll(response.Body)
+			if err != nil {
+				return err
+			}
 		}
 
 		return fmt.Errorf("server response invalid: %s (%s) error message: %s",
-			response.Status, response.Request.URL, error.Error)
+			response.Status, response.Request.URL, errorMsg)
 	}
 	return nil
 }
@@ -55,10 +53,5 @@ func ParseResponseJSON(response *http.Response, dto interface{}) error {
 		return err
 	}
 
-	err = json.Unmarshal(responseJSON, dto)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return json.Unmarshal(responseJSON, dto)
 }
