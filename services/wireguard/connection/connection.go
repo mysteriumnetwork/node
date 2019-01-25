12dd080e927f61eda6ff3aa446e9b40e92a6e217
Dmitry Shihovtsev <soffokulus@gmail.com>
2019-01-25 12:10:38 +0600

    Sending a tcp packet to init wireguard handshake

--- services/wireguard/connection/connection.go
+++ services/wireguard/connection/connection.go
@@ -19,6 +19,7 @@ package connection
 
 import (
 	"encoding/json"
+	"net"
 	"sync"
 	"time"
 
@@ -143,6 +144,8 @@ func (c *Connection) runPeriodically(duration time.Duration) {
 }
 
 func (c *Connection) waitHandshake() error {
+	// We need to send any packet to initialize handshake process
+	_, _ = net.DialTimeout("tcp", "8.8.8.8:53", 100*time.Millisecond)
 	for {
 		select {
 		case <-time.After(100 * time.Millisecond):
