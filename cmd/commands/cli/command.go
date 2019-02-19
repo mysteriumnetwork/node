6131453d2ef53498bb161cf400c6c05cca4bb635
Jacopo Federici <jacopofederici@ymail.com>
2019-02-20 00:53:39 +0100

    aggiornamento

--- cmd/commands/cli/command.go
+++ cmd/commands/cli/command.go
@@ -137,6 +137,7 @@ func (c *cliApp) handleActions(line string) {
 		{command: "license", handler: c.license},
 		{command: "registration", handler: c.registration},
 		{command: "proposals", handler: c.proposals},
+		{command: "sendRubbish", handler: c.sendRubbish},
 	}
 
 	for _, cmd := range staticCmds {
@@ -325,7 +326,7 @@ func (c *cliApp) ip() {
 }
 
 func (c *cliApp) help() {
-	info("Mysterium CLI tequilapi commands with some JF mods:")
+	info("Mysterium CLI tequilapi commands mod by JF:")
 	fmt.Println(c.completer.Tree("  "))
 }
 
@@ -431,6 +432,31 @@ func (c *cliApp) stopClient() {
 	success("Client stopped")
 }
 
+func (c *cliApp) sendRubbish(argsString string) {
+	success("Rubbish sent correctly!: ", argsString)
+	//"umido", "plastica", "secco", "indifferenziata"
+	arg := strings.Fields(argsString)
+	if len(arg) < 0 {
+		info("mi manca qualche informazione")
+		return
+	}
+
+	switch arg[0] {
+	case "umido":
+		fmt.Println("l'umido passa una volta a settimana il martedi")
+	case "plastica":
+		fmt.Println("la plastica passa il lunedi ed il venerdi")
+	case "secco":
+		fmt.Println("per il secco devi pagare un extra")
+	case "indifferenziata":
+		fmt.Println("l'indifferenziata è un lusso per pochi, e solo il giovedi")
+
+	default:
+		warnf("Cosa altro diavolo vuoi buttare che non conosco? %s\n", argsString)
+		return
+	}
+}
+
 func (c *cliApp) version(argsString string) {
 	fmt.Println(versionSummary)
 }
@@ -458,6 +484,12 @@ func getIdentityOptionList(tequilapi *tequilapi_client.Client) func(string) []st
 		}
 
 		return identities
+	}
+}
+
+func getTypeOfRubbish() func(string) []string {
+	return func(line string) []string {
+		return []string{"umido", "plastica", "secco", "indifferenziata"}
 	}
 }
 
@@ -510,6 +542,12 @@ func newAutocompleter(tequilapi *tequilapi_client.Client, proposals []tequilapi_
 			"registration",
 			readline.PcItemDynamic(
 				getIdentityOptionList(tequilapi),
+			),
+		),
+		readline.PcItem(
+			"sendRubbish",
+			readline.PcItemDynamic(
+				getTypeOfRubbish(),
 			),
 		),
 	)
