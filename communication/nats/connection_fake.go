3c84e7782a580f5b1b005c776a3c42d07044c8b4
Dmitry Shihovtsev <soffokulus@gmail.com>
2019-03-25 11:42:04 +0600

    Fixed test for communication channel

--- communication/nats/connection_fake.go
+++ communication/nats/connection_fake.go
@@ -140,6 +140,10 @@ func (conn *connectionFake) Start() {
 	go conn.queueProcessing()
 }
 
+func (conn *connectionFake) Flush() error {
+	return nil
+}
+
 func (conn *connectionFake) Close() {
 	conn.queueShutdown <- true
 }
