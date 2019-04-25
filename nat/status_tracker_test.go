30e7e73591efc2a51dd84803a569fa81bfbff644
Donatas Kucinskas <donce.lt@gmail.com>
2019-04-25 15:11:02 +0300

    Add nat.StatusTracker to track the status of NAT traversal

---
+++ nat/status_tracker_test.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 The "MysteriumNetwork/node" Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package nat
+
+import (
+	"testing"
+
+	"github.com/mysteriumnetwork/node/nat/natevents"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_StatusTracker_Status_ReturnsNotFinishedInitially(t *testing.T) {
+	tracker := NewStatusTracker("last stage")
+	status := tracker.Status()
+	assert.Equal(t, "not_finished", status)
+}
+
+func Test_StatusTracker_Status_ReturnsSuccessful_WithSuccessfulEvent(t *testing.T) {
+	tracker := NewStatusTracker("last stage")
+	tracker.ConsumeNATEvent(natevents.Event{Successful: true, Stage: "any stage"})
+	status := tracker.Status()
+	assert.Equal(t, "successful", status)
+}
+
+func Test_StatusTracker_Status_ReturnsFailure_WithHolepunchingFailureEvent(t *testing.T) {
+	tracker := NewStatusTracker("last stage")
+	tracker.ConsumeNATEvent(natevents.Event{Successful: false, Stage: "last stage"})
+	status := tracker.Status()
+	assert.Equal(t, "failure", status)
+}
+
+func Test_StatusTracker_Status_ReturnsNotFinished_WithPortMappingFailureEvent(t *testing.T) {
+	tracker := NewStatusTracker("last stage")
+	tracker.ConsumeNATEvent(natevents.Event{Successful: false, Stage: "first stage"})
+	status := tracker.Status()
+	assert.Equal(t, "not_finished", status)
+}
