3a9d0b6fec8eb008715eabe226299b678d43bd9e
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-05-10 12:49:08 +0300

    Properly disable ICS on all interfaces
    Manual property patching didn't work here, cause memory corruption errors

--- nat/service_ics.go
+++ nat/service_ics.go
@@ -72,7 +72,17 @@ type serviceICS struct {
 }
 
 func (ics *serviceICS) disableICSAllInterfaces() error {
-	_, err := ics.powerShell(`Get-WmiObject -Class HNet_ConnectionProperties -Namespace "ROOT\microsoft\homenet" | foreach { $_.IsIcsPublic = 0; $_.IsIcsPrivate = 0; $_.Put() };`)
+	// Filter out invalid media (where NETCON_MEDIATYPE = NCM_NONE) because it is not an instance of INetConnection
+	// and will throw a cast error when attempting to `INetSharingConfigurationForINetConnection($conn)`
+	// Enum: https://docs.microsoft.com/en-us/windows/desktop/api/netcon/ne-netcon-tagnetcon_mediatype
+	_, err := ics.powerShell(`regsvr32 /s hnetcfg.dll;
+		$mgr = New-Object -ComObject HnetCfg.HNetShare;
+		filter IsValidMediaType { if ($mgr.NetConnectionProps($_).MediaType -gt 0) { $_ } };
+		$connections = $mgr.EnumEveryConnection | IsValidMediaType;
+		foreach ($conn in $connections) {
+			$mgr.INetSharingConfigurationForINetConnection($conn).DisableSharing()
+		};
+	`)
 	return err
 }
 
