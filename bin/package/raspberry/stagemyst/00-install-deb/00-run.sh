a5b242d84f1d192c510a4b816b47f00096a9eff7
Dmitry Shihovtsev <soffokulus@gmail.com>
2019-05-17 12:00:57 +0600

    Autoupdates for myst on raspberry
    (cherry picked from commit 17c90d0ce7abafaa198f5d3c3b4936e42c376f9a)

--- bin/package/raspberry/stagemyst/00-install-deb/00-run.sh
+++ bin/package/raspberry/stagemyst/00-install-deb/00-run.sh
@@ -4,10 +4,11 @@ install -m 755 files/myst_linux_armhf.deb	"${ROOTFS_DIR}/tmp/"
 
 on_chroot << EOF
 echo "deb http://deb.debian.org/debian/ unstable main" | tee --append /etc/apt/sources.list.d/unstable.list
+echo "deb http://ppa.launchpad.net/mysteriumnetwork/node/ubuntu bionic main " | tee --append /etc/apt/sources.list.d/mysterium.list
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' | tee --append /etc/apt/preferences.d/limit-unstable
 
 apt-get -y install raspberrypi-kernel-headers dirmngr
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553 7638D0442B90D010 04EE7237B7D453EC
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553 7638D0442B90D010 04EE7237B7D453EC ECCB6A56B22C536D
 
 apt-get update
 apt-get -y install wireguard openvpn
@@ -20,6 +21,12 @@ DATA_DIR="--data-dir=/var/lib/mysterium-node"
 DAEMON_OPTS="--tequilapi.address=0.0.0.0"
 SERVICE_OPTS="openvpn"
 confEOF
+
+cat << 'AUTOUPDATE' > /etc/apt/apt.conf.d/51unattended-upgrades-myst
+Unattended-Upgrade::Allowed-Origins {
+  "LP-PPA-mysteriumnetwork-node:bionic";
+};
+AUTOUPDATE
 
 dpkg -i /tmp/myst_linux_armhf.deb
 
