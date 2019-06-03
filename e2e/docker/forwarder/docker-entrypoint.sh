9f031a76e33795861fa70bef783f626662898fc7
Antanas Masevicius <antanas@mysterium.network>
2019-06-03 15:35:58 +0300

    Change to docker compose v2
      - set external NAT on router for containers to have inet connectivity
      - set rp_filter off to enable asynchronous routing
      - set gateway and default route through prepare-run-env.sh script

--- e2e/docker/forwarder/docker-entrypoint.sh
+++ e2e/docker/forwarder/docker-entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 set -e
 
-iptables -t nat -A POSTROUTING -o `ip r get ${GATEWAY} | awk '{ print $3 }'` -j MASQUERADE
+setDefaultRoute() {
+    GW=$1;
+    ip route del default
+    ip route add default via ${GW}
+}
+
+if [ -n "$EXT_NAT" ]; then
+    echo "external NAT for containers via: ${EXT_NAT}"
+    iptables -t nat -A POSTROUTING -o `ip r get ${EXT_NAT} | awk '{ print $3 }'` ! -d 172.16.0.0/12 -j MASQUERADE
+fi
+
+if [ -n "$GATEWAY" ]; then
+    echo "new gateway: ${GATEWAY}"
+    iptables -t nat -A POSTROUTING -o `ip r get ${GATEWAY} | awk '{ print $3 }'` -j MASQUERADE
+    setDefaultRoute ${GATEWAY}
+fi
+
+if [ -n "$DEFAULT_ROUTE" ]; then
+    echo "new default route: ${DEFAULT_ROUTE}"
+    setDefaultRoute ${DEFAULT_ROUTE}
+fi
+
 named -c /etc/bind/named.conf -g -u named
