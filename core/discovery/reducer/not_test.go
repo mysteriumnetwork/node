584e3f896dd3ddbf92d97bcc1ab8e155f36143e1
Waldz <valdas@mysterium.network>
2019-06-03 17:02:20 +0300

    Implement proposal filter - reducer.Not()

---
+++ core/discovery/reducer/not_test.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 The "MysteriumNetwork/node" Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package reducer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Not(t *testing.T) {
+	match := Field(fieldProviderID, func(value interface{}) bool {
+		return value == provider1
+	})
+	match = Not(match)
+
+	assert.True(t, match(proposalEmpty))
+	assert.False(t, match(proposalProvider1Streaming))
+	assert.False(t, match(proposalProvider1Noop))
+	assert.True(t, match(proposalProvider2Streaming))
+}
