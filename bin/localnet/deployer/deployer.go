ea251f18bbea44cf80183d12a2fdacc8caa8c893
Tadas V <tadovas@gmail.com>
2019-06-12 09:37:01 +0300

    Remove whitespace

--- bin/localnet/deployer/deployer.go
+++ bin/localnet/deployer/deployer.go
@@ -25,9 +25,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/mysteriumnetwork/node/blockchain/generated"
