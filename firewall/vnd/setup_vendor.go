21d1db10f57112570e7b06f9510e3ce871a3e830
Tadas Valiukas <tadovas@gmail.com>
2019-06-13 11:48:41 +0300

    Fix copyright year
    Co-Authored-By: Dmitry Shihovtsev <soffokulus@gmail.com>

--- firewall/vnd/setup_vendor.go
+++ firewall/vnd/setup_vendor.go
@@ -1,7 +1,7 @@
 //+build !linux
 
 /*
- * Copyright (C) 2018 The "MysteriumNetwork/node" Authors.
+ * Copyright (C) 2019 The "MysteriumNetwork/node" Authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
