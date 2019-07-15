485fe79b0aed9ee5ce340b659516921c3e13b902
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-07-15 16:31:32 +0300

    Document setup steps

--- bin/package/raspberry/files/1-setup-node.sh
+++ bin/package/raspberry/files/1-setup-node.sh
@@ -3,36 +3,37 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export DEBIAN_FRONTEND="noninteractive"
 
+# Adds APT source once (no repeated entries)
 add_apt_source() {
   local src=$1
   local src_file=$2
-  # Check if the source is missing from source file (do not add repeated entries)
   grep -qF "$src" "$src_file" || echo "$src" | tee -a "$src_file"
 }
 
 # Enable SSH access
 touch /boot/ssh
 
-install --mode=644 default-myst-conf /etc/default/mysterium-node
-
+# Add APT sources
 add_apt_source "deb http://deb.debian.org/debian/ unstable main" "/etc/apt/sources.list.d/unstable.list"
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' | tee --append /etc/apt/preferences.d/limit-unstable
-
 add_apt_source "deb http://ppa.launchpad.net/mysteriumnetwork/node/ubuntu bionic main" "/etc/apt/sources.list.d/mysterium.list"
-
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553 7638D0442B90D010 04EE7237B7D453EC ECCB6A56B22C536D
 apt-get update --allow-releaseinfo-change
+
+# Install myst dependencies
 apt-get -y install \
   wireguard \
   openvpn
 
+# Setup unattended upgrades
 apt-get -y install \
   unattended-upgrades
-
 if [[ "${RELEASE_BUILD}" == "true" ]]; then
   echo "Release build, setting up auto-update"
   install --mode=644 unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 fi
 
+# Install myst
+install --mode=644 default-myst-conf /etc/default/mysterium-node
 chmod 755 myst_linux_armhf.deb
 yes | dpkg -i myst_linux_armhf.deb
