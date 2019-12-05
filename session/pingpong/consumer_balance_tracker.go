1a75d4dc29ea9d7c52d1103bfb5f78168309f6c4
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2019-12-05 19:09:41 +0200

    added consumer balance tracker

---
+++ session/pingpong/consumer_balance_tracker.go
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2019 The "MysteriumNetwork/node" Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pingpong
+
+import (
+	"math/big"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/mysteriumnetwork/node/eventbus"
+	"github.com/mysteriumnetwork/node/identity"
+	"github.com/mysteriumnetwork/node/identity/registry"
+	"github.com/rs/zerolog/log"
+)
+
+// ConsumerBalanceTracker keeps track of consumer balances
+type ConsumerBalanceTracker struct {
+	balances map[identity.Identity]Balance
+
+	mystSCAddress            common.Address
+	consumerBalanceChecker   consumerBalanceChecker
+	channelAddressCalculator channelAddressCalculator
+	lock                     sync.Mutex
+}
+
+// NewConsumerBalanceTracker creates a new instance
+func NewConsumerBalanceTracker(mystSCAddress common.Address, consumerBalanceChecker consumerBalanceChecker, channelAddressCalculator channelAddressCalculator) *ConsumerBalanceTracker {
+	return &ConsumerBalanceTracker{
+		balances:                 make(map[identity.Identity]Balance),
+		consumerBalanceChecker:   consumerBalanceChecker,
+		mystSCAddress:            mystSCAddress,
+		channelAddressCalculator: channelAddressCalculator,
+	}
+}
+
+type consumerBalanceChecker interface {
+	GetConsumerBalance(channel, mystSCAddress common.Address) (*big.Int, error)
+}
+
+// Balance represents the balance
+type Balance struct {
+	BCBalance       uint64
+	CurrentEstimate uint64
+}
+
+// Subscribe subscribes the consumer balance tracker to relevant events
+func (cbt *ConsumerBalanceTracker) Subscribe(bus eventbus.Subscriber) error {
+	err := bus.SubscribeAsync(registry.RegistrationEventTopic, cbt.handleRegistrationEvent)
+	if err != nil {
+		return err
+	}
+	err = bus.SubscribeAsync(ExchangeMessageTopic, cbt.handleExchangeMessageEvent)
+	if err != nil {
+		return err
+	}
+	return bus.SubscribeAsync(identity.IdentityUnlockTopic, cbt.handleUnlockEvent)
+}
+
+// GetBalance gets the current balance for given identity
+func (cbt *ConsumerBalanceTracker) GetBalance(ID identity.Identity) uint64 {
+	cbt.lock.Lock()
+	defer cbt.lock.Unlock()
+	if v, ok := cbt.balances[ID]; ok {
+		return v.CurrentEstimate
+	}
+	return 0
+}
+
+func (cbt *ConsumerBalanceTracker) handleExchangeMessageEvent(event ExchangeMessageEventPayload) {
+	cbt.decreaseBalance(event.Identity, event.AmountPromised)
+}
+
+func (cbt *ConsumerBalanceTracker) handleUnlockEvent(ID identity.Identity) {
+	res, err := cbt.getBCBalance(ID)
+	if err != nil {
+		log.Error().Err(err).Msg("Could not get BC balance")
+		return
+	}
+	cbt.updateBCBalance(ID, res)
+}
+
+func (cbt *ConsumerBalanceTracker) handleRegistrationEvent(event registry.RegistrationEventPayload) {
+	switch event.Status {
+	case registry.RegisteredConsumer, registry.RegisteredProvider:
+		res, err := cbt.getBCBalance(event.ID)
+		if err != nil {
+			log.Error().Err(err).Msg("Could not get BC balance")
+			return
+		}
+		cbt.updateBCBalance(event.ID, res)
+	}
+}
+
+func (cbt *ConsumerBalanceTracker) decreaseBalance(id identity.Identity, b uint64) {
+	cbt.lock.Lock()
+	defer cbt.lock.Unlock()
+	if v, ok := cbt.balances[id]; ok {
+		if v.BCBalance != 0 {
+			v.CurrentEstimate = v.BCBalance - b
+			cbt.balances[id] = v
+		}
+	} else {
+		cbt.balances[id] = Balance{
+			BCBalance:       0,
+			CurrentEstimate: 0,
+		}
+	}
+}
+
+func (cbt *ConsumerBalanceTracker) updateBCBalance(id identity.Identity, b uint64) {
+	cbt.lock.Lock()
+	defer cbt.lock.Unlock()
+	if v, ok := cbt.balances[id]; ok {
+		v.BCBalance = b
+		cbt.balances[id] = v
+	} else {
+		cbt.balances[id] = Balance{
+			BCBalance:       b,
+			CurrentEstimate: b,
+		}
+	}
+}
+
+func (cbt *ConsumerBalanceTracker) getBCBalance(id identity.Identity) (uint64, error) {
+	addr, err := cbt.channelAddressCalculator.GetChannelAddress(id)
+	if err != nil {
+		return 0, err
+	}
+	res, err := cbt.consumerBalanceChecker.GetConsumerBalance(addr, cbt.mystSCAddress)
+	if err != nil {
+		return 0, err
+	}
+	return res.Uint64(), nil
+}
