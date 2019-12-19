e30857b7b4b99ed8b04bdd1d57adb34904f4f0f5
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-12-19 14:11:15 +0200

    Cleanup old log files during node startup
    It will leave no more files than configured with MaxRemain.
    Eventually, there will be MaxRemain*2 files left in log dir, because rolling writer
    does not cleanup files from the previous startups, but I think it's good enough: subsequent startups of node will clean the old files again.

--- logconfig/config_rollingwriter.go
+++ logconfig/config_rollingwriter.go
@@ -19,10 +19,15 @@ package logconfig
 
 import (
 	"io"
+	"io/ioutil"
+	"os"
 	"path"
+	"sort"
+	"strings"
 
 	"github.com/arthurkiller/rollingwriter"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 type rollingWriter struct {
@@ -33,15 +38,49 @@ type rollingWriter struct {
 func newRollingWriter(opts *LogOptions) (writer *rollingWriter, err error) {
 	writer = &rollingWriter{}
 	writer.config = rollingwriter.Config{
-		TimeTagFormat:      "2006.01.02",
-		LogPath:            path.Dir(opts.Filepath),
-		FileName:           path.Base(opts.Filepath),
-		RollingPolicy:      rollingwriter.TimeRolling,
-		RollingTimePattern: "0 0 0 * * *",
-		WriterMode:         "lock",
+		TimeTagFormat:     "20060102T150405",
+		LogPath:           path.Dir(opts.Filepath),
+		FileName:          path.Base(opts.Filepath),
+		RollingPolicy:     rollingwriter.VolumeRolling,
+		RollingVolumeSize: "50MB",
+		Compress:          true,
+		WriterMode:        "lock",
+		MaxRemain:         5,
 	}
 	writer.fw, err = rollingwriter.NewWriterFromConfig(&writer.config)
 	return writer, err
+}
+
+// cleanObsoleteLogs cleans obsolete logs so that the count of remaining log files is equal to w.config.MaxRemain.
+// rollingWriter only handles file rolling at runtime, but if the node is restarted, the count is lost thus we have
+// to do this manually.
+func (w *rollingWriter) cleanObsoleteLogs() error {
+	files, err := ioutil.ReadDir(w.config.LogPath)
+	if err != nil {
+		return err
+	}
+	var oldLogFiles []os.FileInfo
+	baseFilename := w.config.FileName + ".log"
+	for _, file := range files {
+		if strings.Contains(file.Name(), baseFilename) && file.Name() != baseFilename {
+			oldLogFiles = append(oldLogFiles, file)
+		}
+	}
+	if len(oldLogFiles) <= w.config.MaxRemain {
+		log.Debug().Msgf("Found %d old log files in log directory, skipping cleanup", len(oldLogFiles))
+		return nil
+	}
+	log.Info().Msgf("Found %d old log files in log directory, proceeding to cleanup", len(oldLogFiles))
+	sort.Slice(oldLogFiles, func(i, j int) bool {
+		return oldLogFiles[i].ModTime().After(oldLogFiles[j].ModTime())
+	})
+	for i := w.config.MaxRemain; i < len(oldLogFiles); i++ {
+		fp := path.Join(w.config.LogPath, oldLogFiles[i].Name())
+		if err := os.Remove(fp); err != nil {
+			log.Warn().Err(err).Msg("Failed to remove log file: " + fp)
+		}
+	}
+	return nil
 }
 
 func (w *rollingWriter) zeroLogger() io.Writer {
