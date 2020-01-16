100ee7efb769dcf855769fa33acfcd459520ab15
anjmao <andzej.maciusovic@gmail.com>
2020-01-16 17:33:09 +0200

    Cleanup e2e tests infrastructure

--- ci/test/e2e.go
+++ ci/test/e2e.go
@@ -18,177 +18,34 @@
 package test
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/magefile/mage/sh"
+	"github.com/mysteriumnetwork/node/e2e"
 	"github.com/mysteriumnetwork/node/logconfig"
-	"github.com/pkg/errors"
-	"github.com/rs/zerolog/log"
 )
-
-type runner struct {
-	compose         func(args ...string) error
-	etherPassphrase string
-	testEnv         string
-	services        string
-}
 
 // TestE2EBasic runs end-to-end tests
 func TestE2EBasic() error {
 	logconfig.Bootstrap()
 	composeFiles := []string{
-		"bin/localnet/docker-compose.yml",
-		"e2e/docker-compose.yml",
+		"./docker-compose.e2e-basic.yml",
 	}
-	runner := newRunner(composeFiles, "node_e2e_basic_test", "openvpn,noop,wireguard")
-	defer runner.cleanup()
-
-	if err := runner.init(); err != nil {
+	runner, cleanup := e2e.NewRunner(composeFiles, "node_e2e_basic_test", "openvpn,noop,wireguard")
+	defer cleanup()
+	if err := runner.Init(); err != nil {
 		return err
 	}
-
-	return runner.test()
+	return runner.Test()
 }
 
 // TestE2ENAT runs end-to-end tests in NAT environment
 func TestE2ENAT() error {
 	logconfig.Bootstrap()
 	composeFiles := []string{
-		"e2e/traversal/docker-compose.yml",
+		"./docker-compose.e2e-traversal.yml",
 	}
-	runner := newRunner(composeFiles, "node_e2e_nat_test", "openvpn")
-	defer runner.cleanup()
-
-	if err := runner.init(); err != nil {
+	runner, cleanup := e2e.NewRunner(composeFiles, "node_e2e_nat_test", "openvpn")
+	defer cleanup()
+	if err := runner.Init(); err != nil {
 		return err
 	}
-
-	return runner.test()
-}
-
-// TODO this can be merged into runner.test() itself
-func (r *runner) init() error {
-	if err := r.startAppContainers(); err != nil {
-		return err
-	}
-
-	if err := r.startProviderConsumerNodes(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r *runner) startAppContainers() error {
-	log.Info().Msg("Starting other services")
-	if err := r.compose("up", "-d", "broker", "ganache", "ipify"); err != nil {
-		return errors.Wrap(err, "starting other services failed!")
-	}
-	log.Info().Msg("Starting DB")
-	if err := r.compose("up", "-d", "db"); err != nil {
-		return errors.Wrap(err, "starting DB failed!")
-	}
-
-	dbUp := false
-	for start := time.Now(); !dbUp && time.Since(start) < 60*time.Second; {
-		err := r.compose("exec", "-T", "db", "mysqladmin", "ping", "--protocol=TCP", "--silent")
-		if err != nil {
-			log.Info().Msg("Waiting...")
-		} else {
-			log.Info().Msg("DB is up")
-			dbUp = true
-			break
-		}
-	}
-	if !dbUp {
-		return errors.New("starting DB timed out")
-	}
-
-	log.Info().Msg("Starting transactor")
-	if err := r.compose("up", "-d", "transactor"); err != nil {
-		return errors.Wrap(err, "starting transactor failed!")
-	}
-
-	log.Info().Msg("Migrating DB")
-	if err := r.compose("run", "--entrypoint", "bin/db-upgrade", "mysterium-api"); err != nil {
-		return errors.Wrap(err, "migrating DB failed!")
-	}
-
-	log.Info().Msg("Starting mysterium-api")
-	if err := r.compose("up", "-d", "mysterium-api"); err != nil {
-		return errors.Wrap(err, "starting mysterium-api failed!")
-	}
-
-	log.Info().Msg("Deploying contracts")
-	err := r.compose("run", "go-runner",
-		"go", "run", "bin/localnet/deployer/deployer.go",
-		"--keystore.directory=bin/localnet/deployer/keystore",
-		"--ether.address=0x354Bd098B4eF8c9E70B7F21BE2d455DF559705d7",
-		fmt.Sprintf("--ether.passphrase=%v", r.etherPassphrase),
-		"--geth.url=ws://ganache:8545")
-	if err != nil {
-		return errors.Wrap(err, "failed to deploy contracts!")
-	}
-
-	log.Info().Msg("starting accountant")
-	if err := r.compose("up", "-d", "accountant"); err != nil {
-		return errors.Wrap(err, "starting accountant failed!")
-	}
-
-	return nil
-}
-
-func (r *runner) startProviderConsumerNodes() error {
-	log.Info().Msg("Building app images")
-	if err := r.compose("build"); err != nil {
-		return errors.Wrap(err, "building app images failed!")
-	}
-
-	log.Info().Msg("Starting app containers")
-	if err := r.compose("up", "-d", "myst-provider", "myst-consumer"); err != nil {
-		return errors.Wrap(err, "starting app containers failed!")
-	}
-	return nil
-}
-
-func (r *runner) test() error {
-	log.Info().Msg("Running tests for env: " + r.testEnv)
-
-	err := r.compose("run", "go-runner",
-		"go", "test", "-v", "./e2e/...", "-args",
-		"--deployer.keystore-directory=../bin/localnet/deployer/keystore",
-		"--deployer.address=0x354Bd098B4eF8c9E70B7F21BE2d455DF559705d7",
-		"--deployer.passphrase", r.etherPassphrase,
-		"--provider.tequilapi-host=myst-provider",
-		"--provider.tequilapi-port=4050",
-		"--consumer.tequilapi-host=myst-consumer",
-		"--consumer.tequilapi-port=4050",
-		"--geth.url=ws://ganache:8545",
-		"--consumer.services", r.services,
-	)
-	return errors.Wrap(err, "tests failed!")
-}
-
-func (r *runner) cleanup() {
-	log.Info().Msg("Cleaning up")
-	_ = r.compose("logs")
-	if err := r.compose("down", "--volumes", "--remove-orphans", "--timeout", "30"); err != nil {
-		log.Warn().Err(err).Msg("Cleanup error")
-	}
-}
-
-func newRunner(composeFiles []string, testEnv, services string) *runner {
-	fileArgs := make([]string, 0)
-	for _, f := range composeFiles {
-		fileArgs = append(fileArgs, "-f", f)
-	}
-	var args []string
-	args = append(args, fileArgs...)
-	args = append(args, "-p", testEnv)
-
-	return &runner{
-		compose:  sh.RunCmd("docker-compose", args...),
-		testEnv:  testEnv,
-		services: services,
-	}
+	return runner.Test()
 }
---
+++ e2e/runner.go
@@ -0,0 +1,158 @@
+package e2e
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/magefile/mage/sh"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+)
+
+// NewRunner returns e2e test runners instance
+func NewRunner(composeFiles []string, testEnv, services string) (runner *Runner, cleanup func()) {
+	fileArgs := make([]string, 0)
+	for _, f := range composeFiles {
+		fileArgs = append(fileArgs, "-f", f)
+	}
+	var args []string
+	args = append(args, fileArgs...)
+	args = append(args, "-p", testEnv)
+
+	runner = &Runner{
+		compose:  sh.RunCmd("docker-compose", args...),
+		testEnv:  testEnv,
+		services: services,
+	}
+	return runner, runner.cleanup
+}
+
+// Runner is e2e tests runner responsible for starting test environment and running e2e tests.
+type Runner struct {
+	compose         func(args ...string) error
+	etherPassphrase string
+	testEnv         string
+	services        string
+}
+
+// Init initialises containers.
+func (r *Runner) Init() error {
+	if err := r.buildNode(); err != nil {
+		return err
+	}
+
+	if err := r.startAppContainers(); err != nil {
+		return err
+	}
+
+	if err := r.startProviderConsumerNodes(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Test runs e2e tests.
+func (r *Runner) Test() error {
+	log.Info().Msg("Running tests for env: " + r.testEnv)
+
+	err := r.compose("run", "go-runner",
+		"go", "test", "-v", "./e2e/...", "-args",
+		"--deployer.keystore-directory=../e2e/blockchain/keystore",
+		"--deployer.address=0x354Bd098B4eF8c9E70B7F21BE2d455DF559705d7",
+		"--deployer.passphrase", r.etherPassphrase,
+		"--provider.tequilapi-host=myst-provider",
+		"--provider.tequilapi-port=4050",
+		"--consumer.tequilapi-host=myst-consumer",
+		"--consumer.tequilapi-port=4050",
+		"--geth.url=ws://ganache:8545",
+		"--consumer.services", r.services,
+	)
+	return errors.Wrap(err, "tests failed!")
+}
+
+func (r *Runner) cleanup() {
+	log.Info().Msg("Cleaning up")
+	_ = r.compose("logs")
+	if err := r.compose("down", "--volumes", "--remove-orphans", "--timeout", "30"); err != nil {
+		log.Warn().Err(err).Msg("Cleanup error")
+	}
+}
+
+func (r *Runner) startAppContainers() error {
+	log.Info().Msg("Starting other services")
+	if err := r.compose("up", "-d", "broker", "ganache", "ipify"); err != nil {
+		return errors.Wrap(err, "starting other services failed!")
+	}
+	log.Info().Msg("Starting DB")
+	if err := r.compose("up", "-d", "db"); err != nil {
+		return errors.Wrap(err, "starting DB failed!")
+	}
+
+	dbUp := false
+	for start := time.Now(); !dbUp && time.Since(start) < 60*time.Second; {
+		err := r.compose("exec", "-T", "db", "mysqladmin", "ping", "--protocol=TCP", "--silent")
+		if err != nil {
+			log.Info().Msg("Waiting...")
+		} else {
+			log.Info().Msg("DB is up")
+			dbUp = true
+			break
+		}
+	}
+	if !dbUp {
+		return errors.New("starting DB timed out")
+	}
+
+	log.Info().Msg("Starting transactor")
+	if err := r.compose("up", "-d", "transactor"); err != nil {
+		return errors.Wrap(err, "starting transactor failed!")
+	}
+
+	log.Info().Msg("Migrating DB")
+	if err := r.compose("run", "--entrypoint", "bin/db-upgrade", "mysterium-api"); err != nil {
+		return errors.Wrap(err, "migrating DB failed!")
+	}
+
+	log.Info().Msg("Starting mysterium-api")
+	if err := r.compose("up", "-d", "mysterium-api"); err != nil {
+		return errors.Wrap(err, "starting mysterium-api failed!")
+	}
+
+	log.Info().Msg("Deploying contracts")
+	err := r.compose("run", "go-runner",
+		"go", "run", "./e2e/blockchain/deployer.go",
+		"--keystore.directory=./e2e/blockchain/keystore",
+		"--ether.address=0x354Bd098B4eF8c9E70B7F21BE2d455DF559705d7",
+		fmt.Sprintf("--ether.passphrase=%v", r.etherPassphrase),
+		"--geth.url=ws://ganache:8545")
+	if err != nil {
+		return errors.Wrap(err, "failed to deploy contracts!")
+	}
+
+	log.Info().Msg("starting accountant")
+	if err := r.compose("up", "-d", "accountant"); err != nil {
+		return errors.Wrap(err, "starting accountant failed!")
+	}
+
+	return nil
+}
+
+func (r *Runner) startProviderConsumerNodes() error {
+	log.Info().Msg("Building app images")
+	if err := r.compose("build"); err != nil {
+		return errors.Wrap(err, "building app images failed!")
+	}
+
+	log.Info().Msg("Starting app containers")
+	if err := r.compose("up", "-d", "myst-provider", "myst-consumer"); err != nil {
+		return errors.Wrap(err, "starting app containers failed!")
+	}
+	return nil
+}
+
+func (r *Runner) buildNode() error {
+	return r.compose("run",
+		"-e", "GOOS=linux",
+		"go-runner", "sh", "./bin/build",
+	)
+}
