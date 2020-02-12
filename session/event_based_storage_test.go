5ada22e76557abb3deb204d103dbbe8d8c0a604f
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-02-12 08:13:02 +0200

    Improve test stability (#1683)
    Replaced homegrown retry logic with assert.Eventually where async behavior was being tested.

--- session/event_based_storage_test.go
+++ session/event_based_storage_test.go
@@ -26,91 +26,39 @@ import (
 )
 
 func TestEventBasedStorage_PublishesEventsOnCreate(t *testing.T) {
-	instance := expectedSession
+	session := expectedSession
 	mp := &mockPublisher{}
 	sessionStore := NewEventBasedStorage(mp, NewStorageMemory())
 
-	sessionStore.Add(instance)
+	sessionStore.Add(session)
 
-	// since we're shooting the event in an asynchronous fashion, try every millisecond to see if we already have it
-	attempts := 0
-	for range time.After(time.Millisecond) {
-		if attempts > 50 {
-			assert.Fail(t, "no change after a 50 attempts")
-			break
-		}
-		attempts++
-		if mp.getLast().Action == sessionEvent.Created {
-			break
-		}
-	}
-
-	assert.Equal(t, sessionEvent.Payload{
-		Action: sessionEvent.Created,
-		ID:     string(expectedID),
-	}, mp.published)
+	assert.Eventually(t, lastEventMatches(mp, session.ID, sessionEvent.Created), 100*time.Millisecond, 1*time.Millisecond)
 }
 
 func TestEventBasedStorage_PublishesEventsOnDelete(t *testing.T) {
-	instance := expectedSession
+	session := expectedSession
 	mp := &mockPublisher{}
 	sessionStore := NewEventBasedStorage(mp, NewStorageMemory())
-	sessionStore.Add(instance)
+	sessionStore.Add(session)
 
 	time.Sleep(time.Millisecond * 5)
 
-	sessionStore.Remove(instance.ID)
+	sessionStore.Remove(session.ID)
 
-	// since we're shooting the event in an asynchronous fashion, try every microsecond to see if we already have it
-	attempts := 0
-
-	for range time.After(time.Millisecond) {
-		if attempts > 50 {
-			assert.Fail(t, "no change after a 50 attempts")
-			break
-		}
-		attempts++
-		t.Log("attempts", attempts)
-		if mp.getLast().Action == sessionEvent.Removed {
-			break
-		}
-	}
-
-	assert.Equal(t, sessionEvent.Payload{
-		Action: sessionEvent.Removed,
-		ID:     string(expectedID),
-	}, mp.published)
+	assert.Eventually(t, lastEventMatches(mp, session.ID, sessionEvent.Removed), 100*time.Millisecond, 1*time.Millisecond)
 }
 
 func TestEventBasedStorage_PublishesEventsOnDataTransferUpdate(t *testing.T) {
-	instance := expectedSession
+	session := expectedSession
 	mp := &mockPublisher{}
 	sessionStore := NewEventBasedStorage(mp, NewStorageMemory())
-	sessionStore.Add(instance)
+	sessionStore.Add(session)
 
 	time.Sleep(time.Millisecond * 5)
 
-	sessionStore.UpdateDataTransfer(instance.ID, 1, 2)
+	sessionStore.UpdateDataTransfer(session.ID, 1, 2)
 
-	// since we're shooting the event in an asynchronous fashion, try every microsecond to see if we already have it
-	attempts := 0
-
-	for range time.After(time.Millisecond) {
-		if attempts > 50 {
-			assert.Fail(t, "no change after a 50 attempts")
-			break
-		}
-		attempts++
-		t.Log("attempts", attempts)
-		if mp.getLast().Action == sessionEvent.Updated {
-			break
-		}
-	}
-
-	assert.Equal(t, sessionEvent.Payload{
-		Action: sessionEvent.Updated,
-		ID:     string(expectedID),
-	}, mp.published)
+	assert.Eventually(t, lastEventMatches(mp, session.ID, sessionEvent.Updated), 100*time.Millisecond, 1*time.Millisecond)
 }
 
 func TestNewEventBasedStorage_HandlesAppEventTokensEarned(t *testing.T) {
@@ -134,39 +82,25 @@ func TestNewEventBasedStorage_HandlesAppEventTokensEarned(t *testing.T) {
 	storedSession, ok = sessionStore.Find(session.ID)
 	assert.True(t, ok)
 	assert.EqualValues(t, 500, storedSession.TokensEarned)
-	assert.Eventually(t, func() bool {
-		evt := mp.getLast()
-		return evt.ID == string(session.ID) && evt.Action == sessionEvent.Updated
-	}, 1*time.Second, 5*time.Millisecond)
+	assert.Eventually(t, lastEventMatches(mp, session.ID, sessionEvent.Updated), 1*time.Second, 5*time.Millisecond)
 }
 
 func TestEventBasedStorage_PublishesEventsOnRemoveForService(t *testing.T) {
-	instance := expectedSession
+	session := expectedSession
 	mp := &mockPublisher{}
 	sessionStore := NewEventBasedStorage(mp, NewStorageMemory())
-	sessionStore.Add(instance)
+	sessionStore.Add(session)
 
 	time.Sleep(time.Millisecond * 5)
 
 	sessionStore.RemoveForService("whatever")
 
-	// since we're shooting the event in an asynchronous fashion, try every microsecond to see if we already have it
-	attempts := 0
+	assert.Eventually(t, lastEventMatches(mp, "", sessionEvent.Removed), 100*time.Millisecond, 1*time.Millisecond)
+}
 
-	for range time.After(time.Millisecond) {
-		if attempts > 50 {
-			assert.Fail(t, "no change after a 50 attempts")
-			break
-		}
-		attempts++
-		t.Log("attempts", attempts)
-		if mp.getLast().Action == sessionEvent.Removed {
-			break
-		}
+func lastEventMatches(mp *mockPublisher, id ID, action sessionEvent.Action) func() bool {
+	return func() bool {
+		evt := mp.getLast()
+		return evt.ID == string(id) && evt.Action == action
 	}
-
-	assert.Equal(t, sessionEvent.Payload{
-		Action: sessionEvent.Removed,
-		ID:     "",
-	}, mp.published)
 }
