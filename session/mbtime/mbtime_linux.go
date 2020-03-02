e8906c8ab24daedd4ccf0a1850d60d2b0c2e659b
anjmao <andzej.maciusovic@gmail.com>
2020-03-02 17:01:55 +0200

    Add monotonic + bootime time package for payments timer tracker

---
+++ session/mbtime/mbtime_linux.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 The "MysteriumNetwork/node" Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mbtime
+
+import "golang.org/x/sys/unix"
+
+func nanotime() (uint64, error) {
+	var ts unix.Timespec
+	if err := unix.ClockGettime(unix.CLOCK_BOOTTIME, &ts); err != nil {
+		if tempSyscallErr(err) {
+			if err := unix.ClockGettime(unix.CLOCK_BOOTTIME, &ts); err != nil {
+				return 0, err
+			}
+		}
+	}
+	return uint64(ts.Sec*1e9 + ts.Nsec), nil
+}
