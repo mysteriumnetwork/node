1503dd76e6754bcdac837519d11f263709aca758
Dmitry Shihovtsev <soffokulus@gmail.com>
2020-04-06 10:42:52 +0600

    Move grand total recovery to unlock

--- session/pingpong/factory.go
+++ session/pingpong/factory.go
@@ -183,7 +183,6 @@ func ExchangeFactoryFunc(
 	channelImplementation string,
 	registryAddress string,
 	eventBus eventbus.EventBus,
-	getConsumerInfo getConsumerInfo,
 	dataLeewayMegabytes uint64) func(paymentInfo session.PaymentInfo,
 	dialog communication.Dialog, channel p2p.Channel,
 	consumer, provider, accountant identity.Identity, proposal market.ServiceProposal, sessionID string) (connection.PaymentIssuer, error) {
@@ -214,7 +213,6 @@ func ExchangeFactoryFunc(
 			ChannelAddressCalculator:  NewChannelAddressCalculator(accountant.Address, channelImplementation, registryAddress),
 			EventBus:                  eventBus,
 			AccountantAddress:         accountant,
-			ConsumerInfoGetter:        getConsumerInfo,
 			SessionID:                 sessionID,
 			DataLeeway:                datasize.MiB * datasize.BitSize(dataLeewayMegabytes),
 		}
