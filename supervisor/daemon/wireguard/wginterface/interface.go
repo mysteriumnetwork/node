bf0a02b484bd459918435cb6ae7721e68b0955e6
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-07-09 14:00:43 +0300

    Merge macOS and windows wg interface; properly cleanup

--- supervisor/daemon/wireguard/wginterface/interface.go
+++ supervisor/daemon/wireguard/wginterface/interface.go
@@ -18,9 +18,15 @@
 package wginterface
 
 import (
+	"bufio"
+	"fmt"
 	"net"
+	"strings"
 
 	"github.com/mysteriumnetwork/node/services/wireguard/connection/dns"
+	"github.com/mysteriumnetwork/node/services/wireguard/wgcfg"
+	"github.com/mysteriumnetwork/node/utils/netutil"
+	"github.com/rs/zerolog/log"
 	"golang.zx2c4.com/wireguard/device"
 )
 
@@ -30,4 +36,114 @@ type WgInterface struct {
 	Device     *device.Device
 	uapi       net.Listener
 	dnsManager dns.Manager
+}
+
+// New creates new WgInterface instance.
+func New(cfg wgcfg.DeviceConfig, uid string) (*WgInterface, error) {
+	tunnel, interfaceName, err := createTunnel(cfg.IfaceName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create TUN device %s: %w", cfg.IfaceName, err)
+	}
+
+	logger := newLogger(device.LogLevelDebug, fmt.Sprintf("(%s) ", interfaceName))
+	logger.Info.Println("Starting wireguard-go version", device.WireGuardGoVersion)
+
+	logger.Info.Println("Starting device")
+	wgDevice := device.NewDevice(tunnel, logger)
+
+	log.Info().Msg("Creating UAPI listener")
+	uapi, err := newUAPIListener(interfaceName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen on UAPI socket: %w", err)
+	}
+
+	log.Info().Msg("Applying interface configuration")
+	if err := wgDevice.IpcSetOperation(bufio.NewReader(strings.NewReader(cfg.Encode()))); err != nil {
+		down(uapi, wgDevice, nil)
+		return nil, fmt.Errorf("could not set device uapi config: %w", err)
+	}
+
+	log.Info().Msg("Bringing device up")
+	wgDevice.Up()
+
+	log.Info().Msg("Configuring network")
+	dnsManager := dns.NewManager()
+	if err := configureNetwork(cfg, dnsManager); err != nil {
+		down(uapi, wgDevice, dnsManager)
+		return nil, fmt.Errorf("could not setup network: %w", err)
+	}
+
+	if err := applySocketPermissions(interfaceName, uid); err != nil {
+		down(uapi, wgDevice, dnsManager)
+		return nil, fmt.Errorf("could not apply socket permissions: %w", err)
+	}
+
+	wgInterface := &WgInterface{
+		Name:       interfaceName,
+		Device:     wgDevice,
+		uapi:       uapi,
+		dnsManager: dnsManager,
+	}
+	log.Info().Msg("Accepting UAPI requests")
+	go wgInterface.accept()
+
+	return wgInterface, nil
+}
+
+// Accept listens for WireGuard configuration changes via user space socket.
+func (a *WgInterface) accept() {
+	for {
+		conn, err := a.uapi.Accept()
+		if err != nil {
+			log.Err(err).Msg("Failed to close UAPI listener")
+			return
+		}
+		go a.Device.IpcHandle(conn)
+	}
+}
+
+func down(uapi net.Listener, d *device.Device, dnsManager dns.Manager) {
+	if uapi != nil {
+		if err := uapi.Close(); err != nil {
+			log.Warn().Err(err).Msg("Could not close uapi socket")
+		}
+	}
+	if d != nil {
+		d.Close()
+	}
+	if dnsManager != nil {
+		if err := dnsManager.Clean(); err != nil {
+			log.Err(err).Msg("Could not clean DNS")
+		}
+	}
+}
+
+// Down closes device and user space api socket.
+func (a *WgInterface) Down() {
+	down(a.uapi, a.Device, a.dnsManager)
+}
+
+func configureNetwork(cfg wgcfg.DeviceConfig, dnsManager dns.Manager) error {
+	if err := netutil.AssignIP(cfg.IfaceName, cfg.Subnet); err != nil {
+		return fmt.Errorf("failed to assign IP address: %w", err)
+	}
+
+	if cfg.Peer.Endpoint != nil {
+		if err := netutil.ExcludeRoute(cfg.Peer.Endpoint.IP); err != nil {
+			return fmt.Errorf("could not exclude route %s: %w", cfg.Peer.Endpoint.IP.String(), err)
+		}
+		if err := netutil.AddDefaultRoute(cfg.IfaceName); err != nil {
+			return fmt.Errorf("could not add default route for %s: %w", cfg.IfaceName, err)
+		}
+	}
+
+	if err := dnsManager.Set(dns.Config{
+		ScriptDir: cfg.DNSScriptDir,
+		IfaceName: cfg.IfaceName,
+		DNS:       cfg.DNS,
+	}); err != nil {
+		return fmt.Errorf("could not set DNS: %w", err)
+	}
+
+	return nil
 }
