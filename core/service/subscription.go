f2636cb9b1ff71e4fcbd8b090a593d3b03274186
Dmitry Shihovtsev <soffokulus@gmail.com>
2020-07-14 10:42:49 +0600

    Fix panic in session creation

--- core/service/subscription.go
+++ core/service/subscription.go
@@ -27,6 +27,7 @@ import (
 	"github.com/mysteriumnetwork/node/core/service/servicestate"
 	"github.com/mysteriumnetwork/node/eventbus"
 	"github.com/mysteriumnetwork/node/identity"
+	"github.com/mysteriumnetwork/node/market"
 	"github.com/mysteriumnetwork/node/p2p"
 	"github.com/mysteriumnetwork/node/pb"
 	"github.com/mysteriumnetwork/node/session"
@@ -35,23 +36,23 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func subscribeSessionCreate(mng *session.Manager, ch p2p.Channel, service Service, eventPublisher eventbus.Publisher) {
+func subscribeSessionCreate(mng *session.Manager, ch p2p.Channel, service Service, eventPublisher eventbus.Publisher, proposal market.ServiceProposal) {
 	ch.Handle(p2p.TopicSessionCreate, func(c p2p.Context) error {
-		var sessionInstance *session.Session
+		var sessionID string
 
 		tracer := trace.NewTracer()
 		sessionCreateTrace := tracer.StartStage("Provider whole session create")
 
 		defer func() {
 			tracer.EndStage(sessionCreateTrace)
-			traceResult := tracer.Finish(eventPublisher, string(sessionInstance.ID))
+			traceResult := tracer.Finish(eventPublisher, string(sessionID))
 			log.Debug().Msgf("Provider connection trace: %s", traceResult)
 
 			eventPublisher.Publish(servicestate.AppTopicServiceSession, connection.AppEventConnectionSession{
 				Status: connection.SessionEndedStatus,
 				SessionInfo: connection.Status{
-					SessionID: sessionInstance.ID,
-					Proposal:  sessionInstance.Proposal,
+					SessionID: session.ID(sessionID),
+					Proposal:  proposal,
 				},
 			})
 		}()
@@ -70,6 +71,9 @@ func subscribeSessionCreate(mng *session.Manager, ch p2p.Channel, service Servic
 		if err != nil {
 			return fmt.Errorf("cannot start session %s: %w", string(sessionInstance.ID), err)
 		}
+
+		sessionID = string(sessionInstance.ID)
+
 		tracer.EndStage(sessionStartTrace)
 
 		eventPublisher.Publish(servicestate.AppTopicServiceSession, connection.AppEventConnectionSession{
