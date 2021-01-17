7a1589e634fb20a864350ffa17412c396ee66505
Mantas Domaševičius <mantas@mysterium.network>
2021-01-17 10:29:36 +0200

    Node supervisor for linux
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- supervisor/install/install_linux.go
+++ supervisor/install/install_linux.go
@@ -18,15 +18,106 @@
 package install
 
 import (
-	"errors"
+	"bytes"
+	"text/template"
+
+	"github.com/rs/zerolog/log"
+
+	"github.com/takama/daemon"
 )
+
+const daemonName = "myst_supervisor"
+const description = "Myst Supervisor"
+const descriptor = `
+[Unit]
+Description={{.Description}}
+
+[Service]
+PIDFile=/run/{{.Name}}.pid
+ExecStartPre=/bin/rm -f /run/{{.Name}}.pid
+ExecStart={{.Path}} {{.Args}}
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+`
 
 // Install installs service for linux. Not implemented yet.
 func Install(options Options) error {
-	return errors.New("not implemented")
+	if !options.valid() {
+		return errInvalid
+	}
+
+	log.Info().Msg("Configuring Myst Supervisor")
+	dmn, err := mystSupervisorDaemon(options)
+	if err != nil {
+		return err
+	}
+
+	log.Info().Msg("Cleaning up previous installation")
+	clean(dmn)
+
+	log.Info().Msg("Installing daemon")
+	output, err := dmn.Install()
+	if err != nil {
+		log.Info().Msg(output)
+		return err
+	}
+
+	output, err = dmn.Start()
+	if err != nil {
+		log.Info().Msg(output)
+		return err
+	}
+	return nil
+}
+
+func mystSupervisorDaemon(options Options) (daemon.Daemon, error) {
+	dmn, err := daemon.New(daemonName, description, daemon.SystemDaemon)
+	if err != nil {
+		return nil, err
+	}
+
+	t, err := template.New("unit-descriptor").Parse(description)
+	if err != nil {
+		return nil, err
+	}
+	buffer := new(bytes.Buffer)
+	err = t.Execute(buffer, map[string]string{
+		"Path":        options.SupervisorPath,
+		"Description": "{{.Description}}",
+		"Name":        "{{.Name}}",
+		"Args":        "{{.Args}}",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = dmn.SetTemplate(descriptor)
+	if err != nil {
+		return nil, err
+	}
+	return dmn, err
+}
+
+func clean(d daemon.Daemon) error {
+	output, err := d.Stop()
+	if err != nil {
+		log.Info().Msgf("%s\t%s", output, err)
+	}
+	output, err = d.Remove()
+	if err != nil {
+		log.Info().Msgf("%s\t%s", output, err)
+	}
+	return err
 }
 
 // Uninstall installs service for linux. Not implemented yet.
 func Uninstall() error {
-	return errors.New("not implemented")
+	log.Info().Msg("Uninstalling Myst Supervisor daemon")
+	dmn, err := daemon.New(daemonName, description, daemon.SystemDaemon)
+	if err != nil {
+		return err
+	}
+	return clean(dmn)
 }
