bb3964f9148d3429abe48b6889425cc5f24117a9
Dmitry Shihovtsev <soffokulus@gmail.com>
2021-07-02 14:23:22 +0600

    Add TODO to remove docker config migration

--- bin/docker/docker-entrypoint.sh
+++ bin/docker/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 export OS_DIR_DATA="/var/lib/mysterium-node"
 export OS_DIR_RUN="/var/run/mysterium-node"
 
+# TODO remove this block once all container migrated to new config path.
 export OS_DIR_CONFIG="/etc/mysterium-node"
 if [ -f $OS_DIR_CONFIG/config.toml ]; then
     mv $OS_DIR_CONFIG/config.toml $OS_DIR_DATA
