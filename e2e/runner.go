cb2ac4b4df90f91a90d8e0dbbb835cff1f447cbc
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2021-08-04 14:19:19 +0300

    Bump transactor in e2e to 1.0.1

--- e2e/runner.go
+++ e2e/runner.go
@@ -156,7 +156,7 @@ func (r *Runner) Init() error {
 	}
 
 	log.Info().Msg("Starting transactor")
-	if err := r.compose("up", "-d", "transactor"); err != nil {
+	if err := r.compose("up", "-d", "transactor", "transactor-sidecar"); err != nil {
 		return errors.Wrap(err, "starting transactor failed!")
 	}
 
