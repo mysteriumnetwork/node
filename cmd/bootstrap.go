4751af1a68ccb1715753d01a4a1386518eae652d
Mantas Domaševičius <mantas@mysterium.network>
2021-08-24 08:53:12 +0300

    Bootstrap endpoints via Gin Gonic API
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- cmd/bootstrap.go
+++ cmd/bootstrap.go
@@ -21,17 +21,23 @@ package cmd
 
 import (
 	"net"
+	"os"
+	"time"
+
+	"github.com/mysteriumnetwork/node/services"
+	"github.com/mysteriumnetwork/node/utils"
 
 	"github.com/mysteriumnetwork/node/consumer/entertainment"
+
+	"github.com/gin-gonic/gin"
+
 	"github.com/mysteriumnetwork/node/ui"
 	uinoop "github.com/mysteriumnetwork/node/ui/noop"
 
 	"github.com/mysteriumnetwork/node/config"
 	"github.com/mysteriumnetwork/node/core/node"
-	"github.com/mysteriumnetwork/node/services"
 	"github.com/mysteriumnetwork/node/tequilapi"
 	tequilapi_endpoints "github.com/mysteriumnetwork/node/tequilapi/endpoints"
-	"github.com/mysteriumnetwork/node/utils"
 )
 
 func (di *Dependencies) bootstrapTequilapi(nodeOptions node.Options, listener net.Listener) (tequilapi.APIServer, error) {
@@ -39,41 +45,51 @@ func (di *Dependencies) bootstrapTequilapi(nodeOptions node.Options, listener ne
 		return tequilapi.NewNoopAPIServer(), nil
 	}
 
-	router := tequilapi.NewAPIRouter()
-	tequilapi_endpoints.AddRoutesForDocs(router)
-	tequilapi_endpoints.AddRouteForStop(router, utils.SoftKiller(di.Shutdown))
-	tequilapi_endpoints.AddRoutesForAuthentication(router, di.Authenticator, di.JWTAuthenticator)
-	tequilapi_endpoints.AddRoutesForIdentities(router, di.IdentityManager, di.IdentitySelector, di.IdentityRegistry, di.ConsumerBalanceTracker, di.AddressProvider, di.HermesChannelRepository, di.BCHelper, di.Transactor, di.BeneficiaryProvider, di.IdentityMover, di.PayoutAddressStorage)
-	tequilapi_endpoints.AddRoutesForConnection(router, di.ConnectionManager, di.StateKeeper, di.ProposalRepository, di.IdentityRegistry, di.EventBus, di.AddressProvider)
-	tequilapi_endpoints.AddRoutesForSessions(router, di.SessionStorage)
-	tequilapi_endpoints.AddRoutesForConnectionLocation(router, di.IPResolver, di.LocationResolver, di.LocationResolver)
-	tequilapi_endpoints.AddRoutesForProposals(router, di.ProposalRepository, di.PricingHelper, di.LocationResolver, di.FilterPresetStorage, di.NATProber)
-	tequilapi_endpoints.AddRoutesForService(router, di.ServicesManager, services.JSONParsersByType, di.ProposalRepository)
-	tequilapi_endpoints.AddRoutesForAccessPolicies(di.HTTPClient, router, config.GetString(config.FlagAccessPolicyAddress))
-	tequilapi_endpoints.AddRoutesForNAT(router, di.StateKeeper, di.NATProber)
-	tequilapi_endpoints.AddRoutesForTransactor(router, di.IdentityRegistry, di.Transactor, di.HermesPromiseSettler, di.SettlementHistoryStorage, di.AddressProvider)
-	tequilapi_endpoints.AddRoutesForConfig(router)
-	tequilapi_endpoints.AddRoutesForMMN(router, di.MMN)
-	tequilapi_endpoints.AddRoutesForFeedback(router, di.Reporter)
-	tequilapi_endpoints.AddRoutesForConnectivityStatus(router, di.SessionConnectivityStatusStorage)
-	tequilapi_endpoints.AddRoutesForCurrencyExchange(router, di.PilvytisAPI)
-	tequilapi_endpoints.AddRoutesForPilvytis(router, di.PilvytisAPI)
-	tequilapi_endpoints.AddRoutesForTerms(router)
-	tequilapi_endpoints.AddEntertainmentRoutes(router, entertainment.NewEstimator(
-		config.FlagPaymentPriceGiB.Value,
-		config.FlagPaymentPriceHour.Value,
-	))
-	tequilapi_endpoints.AddRoutesForValidator(router)
-	if err := tequilapi_endpoints.AddRoutesForSSE(router, di.StateKeeper, di.EventBus); err != nil {
-		return nil, err
-	}
-
-	if config.GetBool(config.FlagPProfEnable) {
-		tequilapi_endpoints.AddRoutesForPProf(router)
-	}
-
-	corsPolicy := tequilapi.NewMysteriumCorsPolicy()
-	return tequilapi.NewServer(listener, router, corsPolicy), nil
+	return tequilapi.NewServer(
+		listener,
+		[]func(engine *gin.Engine) error{
+			func(e *gin.Engine) error {
+				if err := tequilapi_endpoints.AddRoutesForSSE(e, di.StateKeeper, di.EventBus); err != nil {
+					return err
+				}
+				return nil
+			},
+			func(e *gin.Engine) error {
+				if config.GetBool(config.FlagPProfEnable) {
+					tequilapi_endpoints.AddRoutesForPProf(e)
+				}
+				return nil
+			},
+			func(e *gin.Engine) error {
+				e.GET("/healthcheck", tequilapi_endpoints.HealthCheckEndpointFactory(time.Now, os.Getpid).HealthCheck)
+				return nil
+			},
+			tequilapi_endpoints.AddRouteForStop(utils.SoftKiller(di.Shutdown)),
+			tequilapi_endpoints.AddRoutesForAuthentication(di.Authenticator, di.JWTAuthenticator),
+			tequilapi_endpoints.AddRoutesForIdentities(di.IdentityManager, di.IdentitySelector, di.IdentityRegistry, di.ConsumerBalanceTracker, di.AddressProvider, di.HermesChannelRepository, di.BCHelper, di.Transactor, di.BeneficiaryProvider, di.IdentityMover, di.PayoutAddressStorage),
+			tequilapi_endpoints.AddRoutesForConnection(di.ConnectionManager, di.StateKeeper, di.ProposalRepository, di.IdentityRegistry, di.EventBus, di.AddressProvider),
+			tequilapi_endpoints.AddRoutesForSessions(di.SessionStorage),
+			tequilapi_endpoints.AddRoutesForConnectionLocation(di.IPResolver, di.LocationResolver, di.LocationResolver),
+			tequilapi_endpoints.AddRoutesForProposals(di.ProposalRepository, di.PricingHelper, di.LocationResolver, di.FilterPresetStorage, di.NATProber),
+			tequilapi_endpoints.AddRoutesForService(di.ServicesManager, services.JSONParsersByType, di.ProposalRepository),
+			tequilapi_endpoints.AddRoutesForAccessPolicies(di.HTTPClient, config.GetString(config.FlagAccessPolicyAddress)),
+			tequilapi_endpoints.AddRoutesForNAT(di.StateKeeper, di.NATProber),
+			tequilapi_endpoints.AddRoutesForTransactor(di.IdentityRegistry, di.Transactor, di.HermesPromiseSettler, di.SettlementHistoryStorage, di.AddressProvider),
+			tequilapi_endpoints.AddRoutesForConfig,
+			tequilapi_endpoints.AddRoutesForMMN(di.MMN),
+			tequilapi_endpoints.AddRoutesForFeedback(di.Reporter),
+			tequilapi_endpoints.AddRoutesForConnectivityStatus(di.SessionConnectivityStatusStorage),
+			tequilapi_endpoints.AddRoutesForDocs, // TODO smt wrong
+			tequilapi_endpoints.AddRoutesForCurrencyExchange(di.PilvytisAPI),
+			tequilapi_endpoints.AddRoutesForPilvytis(di.PilvytisAPI),
+			tequilapi_endpoints.AddRoutesForTerms,
+			tequilapi_endpoints.AddEntertainmentRoutes(entertainment.NewEstimator(
+				config.FlagPaymentPriceGiB.Value,
+				config.FlagPaymentPriceHour.Value,
+			)),
+			tequilapi_endpoints.AddRoutesForValidator,
+		},
+	)
 }
 
 func (di *Dependencies) bootstrapUIServer(options node.Options) (err error) {
