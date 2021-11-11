423da8b3834e0c765b445a691ef6ab446c7cffe3
Mantas Domaševičius <mantas@mysterium.network>
2021-11-11 17:52:40 +0200

    Remove TODO
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

--- metadata/network.go
+++ metadata/network.go
@@ -88,8 +88,8 @@ var MainnetDefinition = NetworkDefinition{
 			"https://polygon-rpc.com/",
 		},
 	},
-	MMNAddress:      "https://mystnodes.com",        // TODO: Figure this out
-	MMNAPIAddress:   "https://mystnodes.com/api/v1", // TODO: Figure this out
+	MMNAddress:      "https://mystnodes.com",
+	MMNAPIAddress:   "https://mystnodes.com/api/v1",
 	PilvytisAddress: "https://pilvytis.mysterium.network",
 	DNSMap: map[string][]string{
 		"discovery.mysterium.network":  {"51.158.129.204"},
