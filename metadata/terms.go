80b3c4d386cd65fbab75aefdc72cf7b995822d1f
Donatas <91055712+Donatas-MN@users.noreply.github.com>
2022-01-17 13:09:51 +0200

    Update T&C texts to newest versions

--- metadata/terms.go
+++ metadata/terms.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The "MysteriumNetwork/node" Authors.
+ * Copyright (C) 2022 The "MysteriumNetwork/node" Authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,30 +19,29 @@ package metadata
 
 // CurrentTermsVersion is the version which has to be agreed
 // with when confirming TOS agreements.
-const CurrentTermsVersion = "0.0.34"
+const CurrentTermsVersion = "0.0.35"
 
 const termsAndConditions = `
-                          MYSTERIUM NETWORK BETA
+                            MYSTERIUM NETWORK
                                TERMS OF USE
                            FOR MYSTERIUM NODES
 
 SHORT VERSION IN HUMAN LANGUAGE FOR EXIT NODE:
- - You will join our network and act as an exit node for others who will use your hardware and internet traffic to access internet resources.
+ - You will join our network and act as an exit node for others who will use your hardware and internet traffic to access internet resources.  
  - We strive to protect you from any legal liability and believe that you should not be liable for running an exit node, but we cannot guarantee that you might not face it.
  - You cannot use or encourage or assist others to use the network in any way which violates any laws or rights of any party, including intellectual property rights.
  - You can monitor data which passes through your equipment only to whitelist the exit destinations for outgoing traffic.
  - You can use the network with our Mysterium software ([https://github.com/mysteriumnetwork](https://github.com/mysteriumnetwork)) or any other compatible software.
- - Your use of the network is at your own risk. We provide it to you “as is” without any warranties.
- - In no event will we be liable for any damages which you may incur.
+ - Your use of the network is at your own risk. We provide it to you “as is” without any warranties. 
+ - In no event will we be liable for any damages which you may incur. 
  - We recommend that you read the full version below to get acquainted with all of the terms and conditions.
 
 SHORT VERSION IN HUMAN LANGUAGE FOR END USER:
- - Mysterium Network is still in beta stage. We will do our best to develop it further, but we cannot guarantee this.
- - You cannot use or encourage or assist others to use the network in any way which violates any laws or rights of any party, including intellectual property rights.
- - Your use of the network is at your own risk. We provide it to you “as is” without any warranties.
- - You are responsible for all the content that you receive, distribute or otherwise transmit through the network.
- - You can use the network with our Mysterium software ([https://github.com/mysteriumnetwork/](https://github.com/mysteriumnetwork/)) or any other compatible software.
- - In no event will we be liable for any damages which you may incur.
+ - You cannot use or encourage or assist others to use the network in any way which violates any laws or rights of any party, including intellectual property rights. 
+ - Your use of the network is at your own risk. We provide it to you “as is” without any warranties. 
+ - You are responsible for all the content that you receive, distribute or otherwise transmit through the network. 
+ - You can use the network with our Mysterium software ([https://github.com/mysteriumnetwork](https://github.com/mysteriumnetwork)) or any other compatible software.
+ - In no event will we be liable for any damages which you may incur. 
  - We recommend you to read the full version bellow to get acquainted with all of the terms and conditions.
 
 We recommend that you read the full versions below to get acquainted
