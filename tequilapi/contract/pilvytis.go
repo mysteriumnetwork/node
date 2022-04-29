88e667551956ecf2a5770aa5c226bdd31c217222
Tadas Krivickas <tadas.krivickas@gmail.com>
2022-04-29 16:07:21 +0300

    Support specifying order amount in USD

--- tequilapi/contract/pilvytis.go
+++ tequilapi/contract/pilvytis.go
@@ -210,6 +210,9 @@ type PaymentOrderRequest struct {
 	// example: 3.14
 	MystAmount string `json:"myst_amount"`
 
+	// example: 20
+	AmountUSD string `json:"amount_usd"`
+
 	// example: EUR
 	PayCurrency string `json:"pay_currency"`
 
@@ -229,6 +232,7 @@ func (o *PaymentOrderRequest) GatewayOrderRequest(identity identity.Identity, ga
 		Identity:    identity,
 		Gateway:     gateway,
 		MystAmount:  o.MystAmount,
+		AmountUSD:   o.AmountUSD,
 		PayCurrency: o.PayCurrency,
 		Country:     o.Country,
 		ProjectID:   o.ProjectID,
