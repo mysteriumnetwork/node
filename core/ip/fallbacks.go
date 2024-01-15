6bca1f3b588c88dcc75822ba1113599289f90187
IP2Location <support@ip2location.com>
2024-01-15 14:36:30 +0800

    Update fallbacks.go

--- core/ip/fallbacks.go
+++ core/ip/fallbacks.go
@@ -31,6 +31,7 @@ import (
 var IPFallbackAddresses = []string{
 	"https://api.ipify.org",
 	"https://ip2location.io/ip",
+	"https://ipinfo.io/ip",
 	"https://api.ipify.org",
 	"https://ifconfig.me",
 	"https://www.trackip.net/ip",
