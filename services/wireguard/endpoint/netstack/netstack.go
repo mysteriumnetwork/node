4f88b53ee119a17d9a4ecdd4ddd3b6123b0b525e
Anton Litvinov <jnashicq@gmail.com>
2024-07-24 18:56:44 +0400

    Upgrade gvisor to release-20240422.0 in order to fix data-race (issue #6067)
    Signed-off-by: Anton Litvinov <jnashicq@gmail.com>
    (cherry picked from commit 2be055e4fa5120be22c6671db68f6ca78a83b281)

--- services/wireguard/endpoint/netstack/netstack.go
+++ services/wireguard/endpoint/netstack/netstack.go
@@ -193,7 +193,7 @@ func (tun *netTun) Write(buf [][]byte, offset int) (int, error) {
 
 func (tun *netTun) WriteNotify() {
 	pkt := tun.ep.Read()
-	if pkt.IsNil() {
+	if pkt == nil {
 		return
 	}
 
